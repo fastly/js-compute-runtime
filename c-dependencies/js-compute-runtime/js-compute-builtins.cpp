78add6fc9a6b764cd3ab93bcfdabbaea75bfd25b
Till Schneidereit <till@tillschneidereit.net>
2022-01-28 14:56:14 +0100

    Support Big{Ui,I}nt64Array in crypto.getRandomValues

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -733,7 +733,9 @@ bool is_int_typed_array(JSObject* obj) {
          JS_IsUint16Array(obj) ||
          JS_IsInt32Array(obj) ||
          JS_IsUint32Array(obj) ||
-         JS_IsUint8ClampedArray(obj);
+         JS_IsUint8ClampedArray(obj) ||
+         JS_IsBigInt64Array(obj) ||
+         JS_IsBigUint64Array(obj);
 }
 
 namespace Crypto {
