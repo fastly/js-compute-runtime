b97c988e3e4d09266c3671bbf2ffedae6995b5fd
Trevor Elliott <telliott@fastly.com>
2022-06-01 11:48:21 -0700

    Add a format checking job and clang-format config

---
+++ ci/clang-format.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage() {
+  cat <<EOF
+Usage: $0 [-h] [--fix]
+
+  Format c/c++ source that's tracked by git. Exits with a non-zero return code
+  when formatting is applied.
+
+  -h     Print this message
+  --fix  Format files in-place
+EOF
+}
+
+fix=
+while [ "$#" -gt 0 ] ; do
+  case $1 in
+
+    -h)
+      usage
+      exit 1
+      ;;
+
+    --fix)
+      fix=1
+      ;;
+
+    *)
+      echo "Unrecognized option: $1"
+      echo
+      usage
+      exit 1
+      ;;
+
+  esac
+
+  shift
+done
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+failure=
+for file in $(git ls-files | grep '\.\(cpp\|h\)$'); do
+  if grep -F -x "$file" ci/clang-format.ignore > /dev/null; then
+    continue
+  fi
+
+  formatted="${file}.formatted"
+  clang-format "$file" > "$formatted"
+  if ! diff "$file" "$formatted" > /dev/null; then
+    if [ -z "$fix" ]; then
+      rm "$formatted"
+      echo "${file} needs formatting"
+      failure=1
+    else
+      echo "${file} formatted"
+      mv "$formatted" "$file"
+    fi
+  fi
+
+  rm -f "$formatted"
+done
+
+if [ -n "$failure" ]; then
+  exit 1
+fi
