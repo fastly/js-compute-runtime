ba01a63d55f84ed694ec41fb16c32f9892790440
Trevor Elliott <telliott@fastly.com>
2022-06-13 10:41:49 -0700

    Fix shellcheck errors

--- run-local-integration-tests.sh
+++ run-local-integration-tests.sh
@@ -3,14 +3,16 @@
 # Run all Integration Tests
 
 # Exit on any errors
-set -e
+set -euo pipefail
 
-if [ $# -gt 0 ]
-then
+github_token="$1"
+fastly_token="$2"
+
+if [ $# -gt 0 ]; then
   echo "Running act to test integration tests with:"
-  echo "Github Personal Access Token: $1"
-  echo "Fastly Token: $2"
-  act -j sdktest --secret GITHUB_TOKEN=$1 --secret fastly_token=$2
+  echo "Github Personal Access Token: $github_token"
+  echo "Fastly Token: $fastly_token"
+  act -j sdktest --secret GITHUB_TOKEN="github_token" --secret fastly_token="$fastly_token"
 else
   echo "USAGE: ./_test_integration_tests.sh [Github Personal Access Token to clone https://github.com/fastly/compute-sdk-ci-github-action] [Optional, Only if you want to test C@E Environment: Fastly Token for deploying to C@E services]"
 fi
