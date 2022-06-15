1e555c116661ea0957f545868478435afd233210
Jake Champion <jchampion@fastly.com>
2022-06-15 23:06:33 +0100

    remove requirement for fastly.toml file to exist when using the js-compute-runtime cli and no longer create a tarball as these pieces of functionaliy are now handled by the fastly cli itself (https://developer.fastly.com/reference/cli/compute/)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use anyhow::Context;
 use std::fs;
-use std::mem::ManuallyDrop;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use structopt::StructOpt;
-use toml::Value;
 use wizer::Wizer;
 
 #[derive(StructOpt)]
@@ -21,10 +19,6 @@ pub struct Options {
     #[structopt(long, parse(from_os_str))]
     engine_wasm: Option<PathBuf>,
 
-    /// Skip creation of deployable bundle.
-    #[structopt(long)]
-    skip_pkg: bool,
-
     #[structopt(long = "internal-wizening-mode")]
     wizen: bool,
 }
@@ -32,12 +26,6 @@ pub struct Options {
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     let options = Options::from_args();
-
-    if !(options.wizen || options.skip_pkg) && !Path::exists(Path::new("fastly.toml")) {
-        eprintln!("Error: fastly.toml not found.\n");
-        Options::clap().print_help()?;
-        std::process::exit(-1);
-    }
 
     if options.wizen {
         log::debug!("Wizerize subprocess start");
@@ -47,9 +35,6 @@ fn main() -> anyhow::Result<()> {
     }
 
     initialize_js(&options.input, &options.output, options.engine_wasm)?;
-    if !options.skip_pkg {
-        create_bundle(&options.input, &options.output)?;
-    }
 
     Ok(())
 }
@@ -82,58 +67,6 @@ fn initialize_js(input_path: &PathBuf,
     if !status.success() {
         eprintln!("Wizer failed with status: {}", status);
         std::process::exit(-1);
-    }
-
-    Ok(())
-}
-
-fn create_bundle(_input_path: &PathBuf, wasm_path: &PathBuf) -> anyhow::Result<()> {
-    log::debug!("Creating bundle for Fastly deploy");
-    let fastly_toml = fs::read_to_string("fastly.toml")?.parse::<Value>()?;
-    let name = fastly_toml["name"].as_str().unwrap();
-    let name = sanitize_filename::sanitize(&name).replace(" ", "-");
-    log::debug!("sanitized project name: {}", name);
-
-    let tmpdir = ManuallyDrop::new(tempfile::tempdir()?);
-    log::debug!("Bundling in temp directory: {}", tmpdir.path().display());
-    let build_dir = tmpdir.path().join(&name);
-    log::debug!("Build dir: {}", build_dir.display());
-    fs::create_dir(&build_dir)?;
-    let bin_dir = build_dir.join("bin");
-    fs::create_dir(&bin_dir)?;
-
-    fs::copy(wasm_path, bin_dir.join("main.wasm"))?;
-    fs::copy("fastly.toml", &build_dir.join("fastly.toml"))?;
-
-    let pkgdir = Path::new(".").canonicalize()?.join("pkg");
-    if !Path::exists(&pkgdir) {
-        fs::create_dir(&pkgdir)?;
-    }
-
-    let pkgpath = pkgdir.join(format!("{}.tar.gz", name));
-    log::debug!("Creating tarball: {}", pkgpath.display());
-
-    let status = Command::new("tar")
-        .current_dir(tmpdir.path())
-        .arg("-czf")
-        .arg(pkgpath)
-        .arg(name)
-        .status()
-        .expect("Failed to invoke tar");
-
-    if !status.success() {
-        eprintln!("Tar failed with status: {}", status);
-        std::process::exit(-1);
-    }
-
-    if std::env::var("FASTLY_JS_DEBUG_TEMP_DIR").is_ok() {
-        log::debug!(
-            "FASTLY_JS_DEBUG_TEMP_DIR is set; will not clean up {}",
-            tmpdir.path().display()
-        );
-    } else {
-        log::debug!("Cleaning up {}", tmpdir.path().display());
-        drop(ManuallyDrop::into_inner(tmpdir));
     }
 
     Ok(())
