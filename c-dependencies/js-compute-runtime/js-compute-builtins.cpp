09432fcb2d22a90c9bb9eb175f21fd4c4b28ae37
Jake Champion <jchampion@fastly.com>
2022-06-21 16:26:14 +0100

    Allow TextEncoder to be used as a base class to extend from within application javascript

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -5053,19 +5053,6 @@ namespace Slots {
 enum { Count };
 };
 
-JSObject *create(JSContext *cx);
-
-bool constructor(JSContext *cx, unsigned argc, Value *vp) {
-  CTOR_HEADER("TextEncoder", 0);
-
-  RootedObject self(cx, create(cx));
-  if (!self)
-    return false;
-
-  args.rval().setObject(*self);
-  return true;
-}
-
 const unsigned ctor_length = 0;
 
 bool check_receiver(JSContext *cx, HandleValue receiver, const char *method_name);
@@ -5115,10 +5102,19 @@ bool encoding_get(JSContext *cx, unsigned argc, Value *vp) {
 const JSFunctionSpec methods[] = {JS_FN("encode", encode, 1, JSPROP_ENUMERATE), JS_FS_END};
 
 const JSPropertySpec properties[] = {JS_PSG("encoding", encoding_get, JSPROP_ENUMERATE), JS_PS_END};
-
+bool constructor(JSContext *cx, unsigned argc, Value *vp);
 CLASS_BOILERPLATE(TextEncoder)
 
-JSObject *create(JSContext *cx) { return JS_NewObjectWithGivenProto(cx, &class_, proto_obj); }
+bool constructor(JSContext *cx, unsigned argc, Value *vp) {
+  CTOR_HEADER("TextEncoder", 0);
+
+  RootedObject self(cx, JS_NewObjectForConstructor(cx, &class_, args));
+  if (!self)
+    return false;
+
+  args.rval().setObject(*self);
+  return true;
+}
 } // namespace TextEncoder
 
 namespace TextDecoder {
