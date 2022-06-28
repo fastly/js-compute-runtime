85ee1b8c6a714263be1cd9f6cb28666d2a63a703
Trevor Elliott <telliott@fastly.com>
2022-06-28 11:44:53 -0700

    Move the call to webpack into `build-one.sh`

--- integration-tests/js-compute/build-one.sh
+++ integration-tests/js-compute/build-one.sh
@@ -14,7 +14,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 npm ci -s
 
 if [ -f "fixtures/$test/$test.ts" ]; then
-  npm run -s build:test --test="$test"
+  # TODO: we explicitly ignore errors from webpack, as the tests includes uses
+  # of apis that are not yet typed by /sdk/js-compute/index.t.ts
+  npm run webpack -- "./fixtures/$test/$test.ts" \
+    --output-path "./fixtures/$test" \
+    --output-filename "$test.js" || true
 else
   echo "Skipping typescript conversion for fixtures/$test/$test.js"
 fi
