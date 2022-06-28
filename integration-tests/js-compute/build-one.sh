5b1f3d9d9b1b57a80b0688444361618cffa68797
Trevor Elliott <telliott@fastly.com>
2022-06-28 12:44:43 -0700

    Refactor the build-one.sh script (#133)
    * Switch to `npm ci`, which will not update the package-lock.json file
    
    * Move the call to webpack into `build-one.sh`

--- integration-tests/js-compute/build-one.sh
+++ integration-tests/js-compute/build-one.sh
@@ -11,10 +11,14 @@ test="$1"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-npm install -s
+npm ci -s
 
 if [ -f "fixtures/$test/$test.ts" ]; then
-  npm run -s build:test --test="$test"
+  # TODO: we explicitly ignore errors from webpack, as the tests includes uses
+  # of apis that are not yet typed by /sdk/js-compute/index.t.ts
+  npm run webpack -- "./fixtures/$test/$test.ts" \
+    --output-path "./fixtures/$test" \
+    --output-filename "$test.js" || true
 else
   echo "Skipping typescript conversion for fixtures/$test/$test.js"
 fi
