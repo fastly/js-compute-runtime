fed9681f123acd686dac1ae7708aa826617eb760
Trevor Elliott <telliott@fastly.com>
2022-07-13 15:38:16 -0700

    Split the Logger out of js-compute-builtins.cpp

---
+++ c-dependencies/js-compute-runtime/logger.h
@@ -0,0 +1,14 @@
+#ifndef JS_COMPUTE_RUNTIME_LOGGER_H
+#define JS_COMPUTE_RUNTIME_LOGGER_H
+
+#include "builtin.h"
+
+namespace Logger {
+// Register the Logger class.
+bool init_class(JSContext *cx, JS::HandleObject global);
+
+// Create an instance of the logger class.
+JSObject *create(JSContext *cx, const char *name);
+}
+
+#endif
