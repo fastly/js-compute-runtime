874aed1738064e5994bf99f0da14478df92978d1
Jake Champion <jchampion@fastly.com>
2022-07-21 16:18:42 +0100

    Throw TypeError instead of Error if constructor called as regular function.
    Section 3.7.1 of the WebIDL (https://webidl.spec.whatwg.org/#interface-object) specifies in step 1.2 that if a WebIDL interface is called with `NewTarget` undefined (i.e. not as a constructor) then to throw a TypeError. Our implementation was throwing an Error and not a TypeError.
    
    This commit updates our implementation to throw a TypeError with the same message format that is used within Firefox.

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -55,6 +55,29 @@ using JS::MutableHandleValue;
 
 using JS::PersistentRooted;
 
+struct JSErrorFormatString;
+
+enum JSErrNum {
+#define MSG_DEF(name, count, exception, format) name,
+#include "./error-numbers.msg"
+#undef MSG_DEF
+  JSErrNum_Limit
+};
+
+const JSErrorFormatString js_ErrorFormatString[JSErrNum_Limit] = {
+#define MSG_DEF(name, count, exception, format) {#name, format, count, exception},
+#include "./error-numbers.msg"
+#undef MSG_DEF
+};
+
+const JSErrorFormatString *GetErrorMessage(void *userRef, unsigned errorNumber) {
+  if (errorNumber > 0 && errorNumber < JSErrNum_Limit) {
+    return &js_ErrorFormatString[errorNumber];
+  }
+
+  return nullptr;
+}
+
 enum class Mode { PreWizening, PostWizening };
 
 Mode execution_mode = Mode::PreWizening;
@@ -175,7 +198,8 @@ inline bool ThrowIfNotConstructing(JSContext *cx, const CallArgs &args, const ch
   if (args.isConstructing()) {
     return true;
   }
-  JS_ReportErrorASCII(cx, "Constructor %s requires 'new'", builtinName);
+
+  JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_BUILTIN_CTOR_NO_NEW, builtinName);
   return false;
 }
 
