afb6993d0ba9d75f176b498fbcdc0939fcd77c20
Jake Champion <jchampion@fastly.com>
2022-08-08 15:33:29 +0100

    move console namespace into it's own files

---
+++ c-dependencies/js-compute-runtime/builtins/console.cpp
@@ -0,0 +1,41 @@
+#include "console.h"
+
+namespace Console {
+template <const char *prefix, uint8_t prefix_len>
+static bool console_out(JSContext *cx, unsigned argc, JS::Value *vp) {
+  JS::CallArgs args = CallArgsFromVp(argc, vp);
+  size_t msg_len;
+  JS::UniqueChars msg = encode(cx, args.get(0), &msg_len);
+  if (!msg)
+    return false;
+
+  printf("%s: %s\n", prefix, msg.get());
+  fflush(stdout);
+
+  args.rval().setUndefined();
+  return true;
+}
+
+static constexpr char PREFIX_LOG[] = "Log";
+static constexpr char PREFIX_DEBUG[] = "Debug";
+static constexpr char PREFIX_INFO[] = "Info";
+static constexpr char PREFIX_WARN[] = "Warn";
+static constexpr char PREFIX_ERROR[] = "Error";
+
+const JSFunctionSpec methods[] = {
+    JS_FN("log", (console_out<PREFIX_LOG, 3>), 1, JSPROP_ENUMERATE),
+    JS_FN("debug", (console_out<PREFIX_DEBUG, 5>), 1, JSPROP_ENUMERATE),
+    JS_FN("info", (console_out<PREFIX_INFO, 4>), 1, JSPROP_ENUMERATE),
+    JS_FN("warn", (console_out<PREFIX_WARN, 4>), 1, JSPROP_ENUMERATE),
+    JS_FN("error", (console_out<PREFIX_ERROR, 5>), 1, JSPROP_ENUMERATE),
+    JS_FS_END};
+
+bool create(JSContext *cx, JS::HandleObject global) {
+  JS::RootedObject console(cx, JS_NewPlainObject(cx));
+  if (!console)
+    return false;
+  if (!JS_DefineProperty(cx, global, "console", console, JSPROP_ENUMERATE))
+    return false;
+  return JS_DefineFunctions(cx, console, methods);
+}
+} // namespace Console
