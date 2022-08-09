bbf637808ee3e10577d7c7942fbd5b50bfae8e6a
Jake Champion <jchampion@fastly.com>
2022-08-09 11:16:50 +0100

    extract fastly namespace into its own files

---
+++ c-dependencies/js-compute-runtime/builtins/fastly.h
@@ -0,0 +1,40 @@
+#ifndef JS_COMPUTE_RUNTIME_BUILTIN_FASTLY_H
+#define JS_COMPUTE_RUNTIME_BUILTIN_FASTLY_H
+
+#include "builtin.h"
+
+namespace builtins {
+
+class Fastly : public BuiltinNoConstructor<Fastly> {
+private:
+  static bool log(JSContext *cx, unsigned argc, JS::Value *vp);
+
+public:
+  static constexpr const char *class_name = "Fastly";
+
+  static bool debug_logging_enabled;
+
+  static JS::PersistentRooted<JSObject *> env;
+
+  static JS::PersistentRooted<JSObject *> baseURL;
+  static JS::PersistentRooted<JSString *> defaultBackend;
+
+  static const JSFunctionSpec methods[];
+  static const JSPropertySpec properties[];
+
+  static bool dump(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool enableDebugLogging(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool getGeolocationForIpAddress(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool getLogger(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool includeBytes(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool env_get(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool baseURL_get(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool baseURL_set(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool defaultBackend_get(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool defaultBackend_set(JSContext *cx, unsigned argc, JS::Value *vp);
+  static bool create(JSContext *cx, JS::HandleObject global);
+};
+
+} // namespace builtins
+
+#endif
