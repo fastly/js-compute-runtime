f76aa85e5e9b7f8b11cf71ea2202e037571b3a25
Jake Champion <jchampion@fastly.com>
2022-08-19 13:36:37 +0100

    change WorkerLocation to be a class extending from BuiltinImpl

--- c-dependencies/js-compute-runtime/builtins/worker-location.cpp
+++ c-dependencies/js-compute-runtime/builtins/worker-location.cpp
@@ -1,55 +1,49 @@
 #include "builtin.h"
 #include "builtins/url.h"
+#include "builtins/worker-location.h"
 #include "js-compute-builtins.h"
+#include "host_call.h"
 
 /**
  * The `WorkerLocation` builtin, added to the global object as the data property
  * `location`.
  * https://html.spec.whatwg.org/multipage/workers.html#worker-locations
  */
-namespace WorkerLocation {
-namespace Slots {
-enum { Count };
-};
+namespace builtins {
+JS::PersistentRooted<JSObject *> WorkerLocation::url;
 
-static JS::PersistentRooted<JSObject *> url;
-
-const unsigned ctor_length = 1;
-
-bool constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
-  JS_ReportErrorLatin1(cx, "Illegal constructor WorkerLocation");
-  return false;
-}
-
-bool check_receiver(JSContext *cx, JS::HandleValue receiver, const char *method_name);
-
-#define ACCESSOR_GET(field)                                                                        \
-  bool field##_get(JSContext *cx, unsigned argc, JS::Value *vp) {                                      \
-    METHOD_HEADER(0)                                                                               \
-    REQUEST_HANDLER_ONLY("location." #field)                                                       \
-    return URL::field(cx, url, args.rval());                                                       \
+#define WorkerLocation_ACCESSOR_GET(field) \
+  bool field##_get(JSContext *cx, unsigned argc, JS::Value *vp) { \
+    auto result = WorkerLocation::MethodHeaderWithName(0, cx, argc, vp, __func__); \
+    if (result.isErr()) { \
+      return false; \
+    } \
+    auto [args, self] = result.unwrap(); \
+    REQUEST_HANDLER_ONLY("location." #field) \
+    return URL::field(cx, WorkerLocation::url, args.rval()); \
   }
 
-ACCESSOR_GET(href)
-ACCESSOR_GET(origin)
-ACCESSOR_GET(protocol)
-ACCESSOR_GET(host)
-ACCESSOR_GET(hostname)
-ACCESSOR_GET(port)
-ACCESSOR_GET(pathname)
-ACCESSOR_GET(search)
-ACCESSOR_GET(hash)
+WorkerLocation_ACCESSOR_GET(href)
+WorkerLocation_ACCESSOR_GET(origin)
+WorkerLocation_ACCESSOR_GET(protocol)
+WorkerLocation_ACCESSOR_GET(host)
+WorkerLocation_ACCESSOR_GET(hostname)
+WorkerLocation_ACCESSOR_GET(port)
+WorkerLocation_ACCESSOR_GET(pathname)
+WorkerLocation_ACCESSOR_GET(search)
+WorkerLocation_ACCESSOR_GET(hash)
 
-#undef ACCESSOR_GET
+#undef WorkerLocation_ACCESSOR_GET
 
-bool toString(JSContext *cx, unsigned argc, JS::Value *vp) {
+bool WorkerLocation::toString(JSContext *cx, unsigned argc, JS::Value *vp) {
   METHOD_HEADER(0)
   return href_get(cx, argc, vp);
 }
 
-const JSFunctionSpec methods[] = {JS_FN("toString", toString, 0, JSPROP_ENUMERATE), JS_FS_END};
 
-const JSPropertySpec properties[] = {JS_PSG("href", href_get, JSPROP_ENUMERATE),
+const JSFunctionSpec WorkerLocation::methods[] = {JS_FN("toString", toString, 0, JSPROP_ENUMERATE), JS_FS_END};
+
+const JSPropertySpec WorkerLocation::properties[] = {JS_PSG("href", href_get, JSPROP_ENUMERATE),
                                      JS_PSG("origin", origin_get, JSPROP_ENUMERATE),
                                      JS_PSG("protocol", protocol_get, JSPROP_ENUMERATE),
                                      JS_PSG("host", host_get, JSPROP_ENUMERATE),
@@ -61,14 +55,17 @@ const JSPropertySpec properties[] = {JS_PSG("href", href_get, JSPROP_ENUMERATE),
                                      JS_STRING_SYM_PS(toStringTag, "Location", JSPROP_READONLY),
                                      JS_PS_END};
 
-CLASS_BOILERPLATE_CUSTOM_INIT(WorkerLocation)
+bool WorkerLocation::constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
+  JS_ReportErrorLatin1(cx, "Illegal constructor WorkerLocation");
+  return false;
+}
 
-bool init_class(JSContext *cx, JS::HandleObject global) {
+bool WorkerLocation::init_class(JSContext *cx, JS::HandleObject global) {
   if (!init_class_impl(cx, global)) {
     return false;
   }
 
-  url.init(cx);
+  WorkerLocation::url.init(cx);
 
   JS::RootedObject location(cx, JS_NewObjectWithGivenProto(cx, &class_, proto_obj));
   if (!location) {
@@ -77,4 +74,6 @@ bool init_class(JSContext *cx, JS::HandleObject global) {
 
   return JS_DefineProperty(cx, global, "location", location, JSPROP_ENUMERATE);
 }
-} // namespace WorkerLocation
+
+} // namespace builtins
+
