753a1e72b94a46943c6c567a701ee2207e8a5798
Jake Champion <jchampion@fastly.com>
2022-09-16 17:24:22 +0100

    fix: make the distribution of generated numbers between 0 and 1 uniform
    the previous distribution was accidentally between 0 and 0.5 due to running std::abs on an int32_t when converting to a uint32_t which had the affect of 'losing' the upper-half of the numbers able to be stored in the uint32_t

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -4407,7 +4407,7 @@ bool math_random(JSContext *cx, unsigned argc, Value *vp) {
   int32_t storage;
   int32_t *buf = &storage;
   random_get((int32_t)buf, 8);
-  uint32_t value = std::abs(storage);
+  uint32_t value = storage + std::pow(2, 32);
   double newvalue = static_cast<float>(value) / std::powf(2.0, 32.0);
 
   CallArgs args = CallArgsFromVp(argc, vp);
