ca040883ad7a26930926cb8da789987894e75fd6
Jake Champion <me@jakechampion.name>
2022-09-16 17:30:21 +0100

    Update c-dependencies/js-compute-runtime/js-compute-builtins.cpp

--- c-dependencies/js-compute-runtime/js-compute-builtins.cpp
+++ c-dependencies/js-compute-runtime/js-compute-builtins.cpp
@@ -4406,7 +4406,7 @@ bool process_network_io(JSContext *cx) {
 bool math_random(JSContext *cx, unsigned argc, Value *vp) {
   int32_t storage;
   int32_t *buf = &storage;
-  random_get((int32_t)buf, 8);
+  random_get((int32_t)buf, sizeof(storage));
   uint32_t value = storage + std::pow(2, 32);
   double newvalue = static_cast<float>(value) / std::powf(2.0, 32.0);
 
