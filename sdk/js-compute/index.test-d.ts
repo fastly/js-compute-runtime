0c9039762cbec20c9613a7ae9066ae9b0bd7906b
Jake Champion <jchampion@fastly.com>
2022-09-28 13:49:25 +0100

    Add typescript definition for the new Backend class

--- sdk/js-compute/index.test-d.ts
+++ sdk/js-compute/index.test-d.ts
@@ -25,6 +25,29 @@ import {expectError, expectType} from 'tsd';
     expectType<string>(btoa(''));
 }
 
+// Backend
+{
+    expectError(Backend())
+    expectError(new Backend())
+    expectError(new Backend({name: 'eu'}))
+    expectError(new Backend({target: 'www.example.com'}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com'}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', hostOverride: 'example.com', }))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', connectTimeout: 5000, }))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', firstByteTimeout: 5000, }))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', betweenBytesTimeout: 5000, }))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', useSSL: true,}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', tlsMinVersion: 1.2,}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', tlsMaxVersion: 1.2,}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', certificateHostname: 'example.com',}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', checkCertificate: false,}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', caCertificate: '',}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com', ciphers: 'DEFAULT',}))
+    expectType<Backend>(new Backend({name: 'eu', target: 'www.example.com',  sniHostname: 'example.com',}))
+    const backend = new Backend({name: 'eu', target: 'www.example.com'})
+    expectType<string>(backend.toString())
+}
+
 // ObjectStore
 {
     expectError(ObjectStore())
