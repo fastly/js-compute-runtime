b81914198e167332e82dcaed8f09746ecd505c3d
Jake Champion <jchampion@fastly.com>
2022-10-20 14:34:13 +0100

    Perf: Remove IP parsing/validation logic as this is already handled by the host

--- c-dependencies/js-compute-runtime/geo_ip.cpp
+++ c-dependencies/js-compute-runtime/geo_ip.cpp
@@ -1,5 +1,3 @@
-#include <arpa/inet.h>
-
 #include "geo_ip.h"
 #include "host_call.h"
 #include "js-compute-builtins.h" // for encode
@@ -8,24 +6,14 @@
 JSString *get_geo_info(JSContext *cx, JS::HandleString address_str) {
   size_t address_len;
   JS::UniqueChars address = encode(cx, address_str, &address_len);
-  if (!address)
+  if (!address) {
     return nullptr;
-
-  // TODO: Remove all of this and rely on the host for validation as the hostcall only takes one
-  // user-supplied parameter
-  int format = AF_INET;
-  size_t octets_len = 4;
-  const char *caddress = address.get();
-  for (size_t i = 0; i < address_len; i++) {
-    if (caddress[i] == ':') {
-      format = AF_INET6;
-      octets_len = 16;
-      break;
-    }
   }
 
-  char octets[sizeof(struct in6_addr)];
-  if (inet_pton(format, caddress, octets) != 1) {
+  OwnedHostCallBuffer buffer;
+  size_t nwritten = 0;
+  auto result = convert_to_fastly_status(xqd_geo_lookup(address.get(), address_len, buffer.get(), HOSTCALL_BUFFER_LEN, &nwritten));
+  if (result == FastlyStatus::Inval) {
     // While get_geo_info can be invoked through FetchEvent#client.geo, too,
     // that path can't result in an invalid address here, so we can be more
     // specific in the error message.
@@ -33,11 +21,7 @@ JSString *get_geo_info(JSContext *cx, JS::HandleString address_str) {
     JS_ReportErrorLatin1(cx, "Invalid address passed to fastly.getGeolocationForIpAddress");
     return nullptr;
   }
-
-  OwnedHostCallBuffer buffer;
-  size_t nwritten = 0;
-  if (!HANDLE_RESULT(
-          cx, xqd_geo_lookup(octets, octets_len, buffer.get(), HOSTCALL_BUFFER_LEN, &nwritten))) {
+  if (!HANDLE_RESULT(cx, result)) {
     return nullptr;
   }
 
