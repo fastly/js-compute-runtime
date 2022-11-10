1fc0755c552bfad439a89422aa0e7ae87107803f
Jake Champion <jchampion@fastly.com>
2022-11-10 17:44:09 +0000

    remove unused js modules

--- src/onExitOrError.js
+++
@@ -1,6 +0,0 @@
-export function onExitOrError(childProcess) {
-  return new Promise((resolve, reject) => {
-    childProcess.once("exit", resolve);
-    childProcess.once("error", reject);
-  });
-}
