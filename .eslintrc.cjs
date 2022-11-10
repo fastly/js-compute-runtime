da674566075f346ea4d416c7f26bdfeae19152d8
Jake Champion <jchampion@fastly.com>
2022-11-10 11:51:55 +0000

    precompile regex literals to ensure we get fast regexes when the application is running

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
--- src/compileApplicationToWasm.js
+++ src/compileApplicationToWasm.js
@@ -1,83 +1,143 @@
-import { dirname } from 'node:path'
-import { spawn } from "node:child_process"
-import { mkdir } from "node:fs/promises"
-import { createReadStream } from "node:fs"
-import { isFile } from './isFile.js'
-import { isFileOrDoesNotExist } from './isFileOrDoesNotExist.js'
-import { onOpenOrError } from './onOpenOrError.js'
-import { onExitOrError } from './onExitOrError.js'
-import wizer from '@bytecode-alliance/wizer'
+import { dirname } from "node:path";
+import { spawnSync } from "node:child_process";
+import { mkdir, readFile } from "node:fs/promises";
+import { isFile } from "./isFile.js";
+import { isFileOrDoesNotExist } from "./isFileOrDoesNotExist.js";
+import wizer from "@bytecode-alliance/wizer";
+import Parser, { Query } from "tree-sitter";
+import JavaScript from "tree-sitter-javascript";
+
+function findRegexLiterals(source) {
+  const parser = new Parser();
+  parser.setLanguage(JavaScript);
+
+  const tree = parser.parse(source);
+  const query = new Query(
+    JavaScript,
+    "(regex pattern: (regex_pattern) @pattern flags: (regex_flags)? @flags)"
+  );
+  const regexLiterals = [];
+  for (const m of query.matches(tree.rootNode)) {
+    regexLiterals.push({
+      pattern: m.captures[0].node.text,
+      flags: m.captures[1]?.node.text || "",
+    });
+  }
+  return regexLiterals;
+}
+
+const PREAMBLE = `;{
+  // Precompiled regular expressions
+  const precompile = (r) => { r.exec('a'); r.exec('\\u1000'); };`;
+const POSTAMBLE = "}";
+
+/// Emit a block of javascript that will pre-compile the regular expressions given. As spidermonkey
+/// will intern regular expressions, duplicating them at the top level and testing them with both
+/// an ascii and utf8 string should ensure that they won't be re-compiled when run in the fetch
+/// handler.
+function precompile(literals) {
+  return (
+    PREAMBLE +
+    literals
+      .map((regex) => {
+        return `precompile(/${regex.pattern}/${regex.flags});`;
+      })
+      .join("\n") +
+    POSTAMBLE
+  );
+}
 
 export async function compileApplicationToWasm(input, output, wasmEngine) {
   try {
     if (!(await isFile(input))) {
-      console.error(`Error: The \`input\` path does not point to a file: ${input}`)
-      process.exit(1)
+      console.error(
+        `Error: The \`input\` path does not point to a file: ${input}`
+      );
+      process.exit(1);
     }
   } catch (error) {
-    console.error(`Error: The \`input\` path points to a non-existant file: ${input}`)
-    process.exit(1)
+    console.error(
+      `Error: The \`input\` path points to a non-existant file: ${input}`
+    );
+    process.exit(1);
   }
 
-  const inputContents = createReadStream(input, { encoding: 'utf-8' })
+  let inputContents;
   try {
-    await onOpenOrError(inputContents)
+    inputContents = await readFile(input, { encoding: "utf-8" });
   } catch (error) {
-    console.error("Error: Failed to open the `input` (${input})", error.message)
-    process.exit(1)
+    console.error(
+      "Error: Failed to open the `input` (${input})",
+      error.message
+    );
+    process.exit(1);
   }
-  // TODO: precompile regexes in the input file
-  // let source = fs::read_to_string(input_path)?;
-  // let lits = regex::find_literals(&source);
-
-  // let js_file = if !lits.is_empty() {
-  //     let mut temp = tempfile::tempfile()?;
-  //     writeln!(temp, "{}", &source)?;
-  //     regex::precompile(&lits, &mut temp)?;
-  //     temp.rewind()?;
-  //     temp
-  // } else {
-  //     fs::File::open(input_path)
-  //         .with_context(|| format!("failed to open JS file: {}", input_path.display()))?
-  // };
   // TODO: Enhancement - Check that input is valid JavaScript syntax before Wizening
   try {
     if (!(await isFile(wasmEngine))) {
-      console.error(`Error: The \`wasmEngine\` path does not point to a file: ${wasmEngine}`)
-      process.exit(1)
+      console.error(
+        `Error: The \`wasmEngine\` path does not point to a file: ${wasmEngine}`
+      );
+      process.exit(1);
     }
   } catch (error) {
-    console.error(`Error: The \`wasmEngine\` path points to a non-existant file: ${wasmEngine}`)
-    process.exit(1)
+    console.error(
+      `Error: The \`wasmEngine\` path points to a non-existant file: ${wasmEngine}`
+    );
+    process.exit(1);
   }
   try {
     await mkdir(dirname(output), {
-      recursive: true
-    })
+      recursive: true,
+    });
   } catch (error) {
-    console.error(`Error: Failed to create the \`output\` (${output}) directory`, error.message)
-    process.exit(1)
+    console.error(
+      `Error: Failed to create the \`output\` (${output}) directory`,
+      error.message
+    );
+    process.exit(1);
   }
 
   try {
     if (!(await isFileOrDoesNotExist(output))) {
-      console.error(`Error: The \`output\` path does not point to a file: ${output}`)
-      process.exit(1)
+      console.error(
+        `Error: The \`output\` path does not point to a file: ${output}`
+      );
+      process.exit(1);
     }
   } catch (error) {
-    console.error(`Error: Failed to check whether the \`output\` (${output}) is a file path`, error.message)
-    process.exit(1)
+    console.error(
+      `Error: Failed to check whether the \`output\` (${output}) is a file path`,
+      error.message
+    );
+    process.exit(1);
   }
 
-  const wizerProcess = spawn(wizer, ['--allow-wasi', '--dir=.', '-r _start=wizer.resume', `-o=${output}`, wasmEngine], {
-    stdio: [inputContents, process.stdout, process.stderr],
-    encoding: "utf-8",
-  })
+  let lits = findRegexLiterals(inputContents);
+
+  if (lits.length != 0) {
+    inputContents += precompile(lits);
+  }
+
   try {
-    const code = await onExitOrError(wizerProcess)
-    process.exitCode = code
-  } catch (err) {
-    console.error(`Error: Failed to compile JavaScript to Wasm`, error.message)
-    process.exit(1)
+    let wizerProcess = spawnSync(
+      wizer,
+      [
+        "--allow-wasi",
+        `--dir=${process.cwd()}`,
+        "-r _start=wizer.resume",
+        `-o=${output}`,
+        wasmEngine,
+      ],
+      {
+        stdio: [null, process.stdout, process.stderr],
+        input: inputContents,
+        encoding: "utf-8",
+      }
+    );
+    process.exitCode = wizerProcess.status;
+  } catch (error) {
+    console.error(`Error: Failed to compile JavaScript to Wasm`, error.message);
+    process.exit(1);
   }
 }
