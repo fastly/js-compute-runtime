bcf7f8cd8690b3dc0d93459f6c9eb46808af38dd
Jake Champion <jchampion@fastly.com>
2022-11-21 15:36:16 +0000

    add test for custom output file path

---
+++ integration-tests/cli/custom-output-path.test.js
@@ -0,0 +1,23 @@
+import test from 'brittle';
+import { getBinPath } from 'get-bin-path'
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
+
+const cli = await getBinPath()
+
+test('should create output directory, wasm file and return zero exit code', async function (t) {
+    const { execute, cleanup, writeFile, exists, path } = await prepareEnvironment();
+    t.teardown(async function () {
+        await cleanup();
+    });
+
+    await writeFile('./index.js', `addEventListener('fetch', function(){})`)
+    
+    t.is(await exists('./my/cool/app.wasm'), false)
+
+    const { code, stdout, stderr } = await execute('node', `${cli} ${path}/index.js ${path}/my/cool/app.wasm`);
+
+    t.is(await exists('./my/cool/app.wasm'), true)
+    t.alike(stdout, []);
+    t.alike(stderr, []);
+    t.is(code, 0);
+});
