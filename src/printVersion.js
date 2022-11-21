04e32b2ad17eca01591650015f78e9d2fd45aa43
Jake Champion <jchampion@fastly.com>
2022-11-21 15:30:54 +0000

    fix --version functionality by using the correct path for cli's package.json

--- src/printVersion.js
+++ src/printVersion.js
@@ -4,7 +4,7 @@ import { dirname, join } from "node:path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export async function printVersion() {
-  const packageJson = await readFile(join(__dirname, "/package.json"), {
+  const packageJson = await readFile(join(__dirname, "../package.json"), {
     encoding: "utf-8",
   });
   const version = JSON.parse(packageJson).version;
