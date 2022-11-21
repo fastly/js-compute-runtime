7c1a1fd514d31f0b411b2ecd73c8f177802b3626
Jake Champion <jchampion@fastly.com>
2022-11-21 15:35:53 +0000

    add more tests for error conditions in the cli

---
+++ integration-tests/cli/non-existant-engine-wasm-path.test.js
@@ -0,0 +1,19 @@
+import test from 'brittle';
+import { getBinPath } from 'get-bin-path'
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
+
+const cli = await getBinPath()
+
+test('should return non-zero exit code', async function (t) {
+    const { execute, cleanup, writeFile,path} = await prepareEnvironment();
+    t.teardown(async function () {
+        await cleanup();
+    });
+
+    await writeFile('./bin/index.js', `addEventListener('fetch', function(){})`)
+    const { code, stdout, stderr } = await execute('node', `${cli} --engine-wasm ${path}/engine.wasm`);
+
+    t.alike(stdout, []);
+    t.alike(stderr, ['Error: The `wasmEngine` path points to a non-existant file: {{base}}/engine.wasm']);
+    t.is(code, 1);
+});
---
+++ integration-tests/cli/non-existant-input-path.test.js
@@ -0,0 +1,18 @@
+import test from 'brittle';
+import { getBinPath } from 'get-bin-path'
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
+
+const cli = await getBinPath()
+
+test('should return non-zero exit code', async function (t) {
+    const { execute, cleanup} = await prepareEnvironment();
+    t.teardown(async function () {
+        await cleanup();
+    });
+
+    const { code, stdout, stderr } = await execute('node', cli);
+
+    t.alike(stdout, []);
+    t.alike(stderr, ['Error: The `input` path points to a non-existant file: {{base}}/bin/index.js']);
+    t.is(code, 1);
+});
