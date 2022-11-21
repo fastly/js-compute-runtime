342f9fae85890b30030db1277e986c9a7bd64cd8
Jake Champion <jchampion@fastly.com>
2022-11-21 15:32:02 +0000

    refactor test to ensure cleanup always runs

--- integration-tests/cli/invalid.test.js
+++ integration-tests/cli/invalid.test.js
@@ -4,15 +4,13 @@ import { prepareEnvironment } from '@gmrchk/cli-testing-library';
 
 const cli = await getBinPath()
 
-test('should bail on syntax errors', async function (t) {
+test('should return non-zero exit code on syntax errors', async function (t) {
     const { execute, cleanup, writeFile } = await prepareEnvironment();
+    t.teardown(async function () {
+        await cleanup();
+    });
     await writeFile('./bin/index.js', '@')
-    const { code } = await execute(
-        'node',
-        cli
-    );
-
+    const { code } = await execute('node', cli);
+    
     t.is(code, 1);
-
-    await cleanup();
 });
