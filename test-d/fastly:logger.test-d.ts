f81c413b52641295287ad7672bbb6ce51e12b575
Jake Champion <jchampion@fastly.com>
2022-11-25 16:51:32 +0000

    feature: expose fastly loggers via fastly:logger module

---
+++ test-d/fastly:logger.test-d.ts
@@ -0,0 +1,9 @@
+/// <reference path="../types/fastly:logger.d.ts" />
+import { Logger } from "fastly:logger";
+import { expectError, expectType } from 'tsd';
+
+expectError(new Logger())
+expectError(Logger('example'))
+expectError(Logger())
+expectType<Logger>(new Logger('example'))
+expectType<(message: any) => void>(new Logger('example').log)
