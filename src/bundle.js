57041b8603c28b6b5270fbd13d336b3fb8bf5c15
Jake Champion <jchampion@fastly.com>
2022-11-28 15:21:55 +0000

    bundle using esbuild instead of rollup as esbuild preserves the syntax and rollup modifies the syntax in a way which does not work for web-platform-tests

--- src/bundle.js
+++ src/bundle.js
@@ -1,28 +1,34 @@
-import virtual from '@rollup/plugin-virtual';
-import { rollup } from 'rollup';
+import { build } from 'esbuild'
 
-export async function bundle(input) {
-  try {
-    const bundle = await rollup({
-      input,
-      plugins: [
-        virtual({
-          'fastly:backend': `export const Backend = globalThis.Backend`,
-          'fastly:cache-override': `export const CacheOverride = globalThis.CacheOverride`,
-          'fastly:config-store': `export const ConfigStore = globalThis.ConfigStore`,
-          'fastly:dictionary': `export const Dictionary = globalThis.Dictionary`,
-          'fastly:env': `export const env = globalThis.fastly.env.get`,
-          'fastly:geolocation': `export const getGeolocationForIpAddress = globalThis.fastly.getGeolocationForIpAddress`,
-          'fastly:logger': `export const Logger = globalThis.Logger`,
-          'fastly:object-store': `export const ObjectStore = globalThis.ObjectStore`,
-        })
-      ]
-    });
-    return await bundle.generate({
-      format: 'iife'
+let fastlyPlugin = {
+  name: 'fastly',
+  setup(build) {
+    build.onResolve({ filter: /^fastly:.*/ }, args => {
+      return {
+        path: args.path.replace('fastly:', ''),
+        namespace: 'fastly',
+      }
     })
-  } catch (error) {
-    console.error(error);
-  }
+    build.onLoad({ filter: /^.*/, namespace: 'fastly' }, async (args) => {
+      switch (args.path) {
+        case 'backend': {return {contents:`export const Backend = globalThis.Backend`}}
+        case 'cache-override': {return {contents:`export const CacheOverride = globalThis.CacheOverride`}}
+        case 'config-store': {return {contents:`export const ConfigStore = globalThis.ConfigStore`}}
+        case 'dictionary': {return {contents:`export const Dictionary = globalThis.Dictionary`}}
+        case 'env': {return {contents:`export const env = globalThis.fastly.env.get`}}
+        case 'geolocation': {return {contents:`export const getGeolocationForIpAddress = globalThis.fastly.getGeolocationForIpAddress`}}
+        case 'logger': {return {contents:`export const Logger = globalThis.Logger`}}
+        case 'object-store': {return {contents:`export const ObjectStore = globalThis.ObjectStore`}}
+      }
+    })
+  },
 }
 
+export async function bundle(input) {
+  return await build({
+    entryPoints: [input],
+    bundle: true,
+    write: false,
+    plugins: [fastlyPlugin],
+  })
+}
