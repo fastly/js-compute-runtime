6bb3119daa7b3e074a442a5960bf19175c7aa22e
Jake Champion <jchampion@fastly.com>
2022-11-30 00:16:03 +0000

    update to c++20

--- c-dependencies/js-compute-runtime/js-compute-runtime.cpp
+++ c-dependencies/js-compute-runtime/js-compute-runtime.cpp
@@ -11,7 +11,7 @@
 // TODO: remove these once the warnings are fixed
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Winvalid-offsetof"
-
+#pragma clang diagnostic ignored "-Wdeprecated-enum-enum-conversion"
 #include "js/CompilationAndEvaluation.h"
 #include "js/ContextOptions.h"
 #include "js/Initialization.h"
