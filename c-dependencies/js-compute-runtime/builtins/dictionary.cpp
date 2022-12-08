841b8f005a4834fc62585ec4cc1a4f942d462f75
Guy Bedford <gbedford@fastly.com>
2022-12-07 18:39:32 -0800

    dictionary constructor / open

--- c-dependencies/js-compute-runtime/builtins/dictionary.cpp
+++ c-dependencies/js-compute-runtime/builtins/dictionary.cpp
@@ -65,52 +65,57 @@ bool Dictionary::constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
 
   JS::HandleValue name_arg = args.get(0);
 
-  size_t name_len;
+  xqd_world_string_t name_str;
   // Convert into a String following https://tc39.es/ecma262/#sec-tostring
-  JS::UniqueChars name_chars = encode(cx, name_arg, &name_len);
-  if (!name_chars) {
+  JS::UniqueChars name = encode(cx, name_arg, &name_str.len);
+  if (!name) {
     return false;
   }
+  name_str.ptr = name.get();
 
   // If the converted string has a length of 0 then we throw an Error
   // because Dictionary names have to be at-least 1 character.
-  if (name_len == 0) {
+  if (name_str.len == 0) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_DICTIONARY_NAME_EMPTY);
     return false;
   }
 
   // If the converted string has a length of more than 255 then we throw an Error
   // because Dictionary names have to be less than 255 characters.
-  if (name_len > 255) {
+  if (name_str.len > 255) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_DICTIONARY_NAME_TOO_LONG);
     return false;
   }
 
-  std::string_view name(name_chars.get(), name_len);
-
   // Name must start with ascii alphabetical and contain only ascii alphanumeric, underscore, and
   // whitespace
-  if (!std::isalpha(name.front())) {
+  std::string_view name_view(name_str.ptr, name_str.len);
+
+  if (!std::isalpha(name_view.front())) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                               JSMSG_DICTIONARY_NAME_START_WITH_ASCII_ALPHA);
     return false;
   }
 
-  auto is_valid_name = std::all_of(std::next(name.begin(), 1), name.end(), [&](auto character) {
-    return std::isalnum(character) || character == '_' || character == ' ';
-  });
+  auto is_valid_name =
+      std::all_of(std::next(name_view.begin(), 1), name_view.end(), [&](auto character) {
+        return std::isalnum(character) || character == '_' || character == ' ';
+      });
 
   if (!is_valid_name) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                               JSMSG_DICTIONARY_NAME_CONTAINS_INVALID_CHARACTER);
     return false;
   }
+
   JS::RootedObject dictionary(cx, JS_NewObjectForConstructor(cx, &class_, args));
+
   fastly_dictionary_handle_t dict_handle = INVALID_HANDLE;
-  auto status = convert_to_fastly_status(xqd_dictionary_open(name.data(), name_len, &dict_handle));
+
+  auto status = convert_to_fastly_status(xqd_fastly_dictionary_open(&name_str, &dict_handle));
   if (status == FastlyStatus::BadF) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_DICTIONARY_DOES_NOT_EXIST,
-                              name.data());
+                              name_str.ptr);
     return false;
   }
   if (!HANDLE_RESULT(cx, status)) {
