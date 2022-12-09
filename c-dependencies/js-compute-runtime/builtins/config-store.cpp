57c40bd18a9e971a8a701b2edfa29003ad8c3d04
Jake Champion <jchampion@fastly.com>
2022-12-09 16:03:30 +0000

    formatting fixes

--- c-dependencies/js-compute-runtime/builtins/config-store.cpp
+++ c-dependencies/js-compute-runtime/builtins/config-store.cpp
@@ -97,7 +97,8 @@ bool ConfigStore::constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
   JS::RootedObject config_store(cx, JS_NewObjectForConstructor(cx, &class_, args));
   ConfigStoreHandle dict_handle = {INVALID_HANDLE};
   // TODO: Throw JS Error if the requested store does not exist
-  auto status = convert_to_fastly_status(xqd_config_store_open(name.data(), name_len, &dict_handle));
+  auto status =
+      convert_to_fastly_status(xqd_config_store_open(name.data(), name_len, &dict_handle));
   if (status == FastlyStatus::BadF) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_CONFIG_STORE_DOES_NOT_EXIST,
                               name.data());
