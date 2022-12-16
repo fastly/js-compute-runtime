f026dcfacae097513538d46abf0cf564a4a22700
Jake Champion <jchampion@fastly.com>
2022-12-16 15:35:10 +0000

    chore: add tests to confirm behavior for copying a request is working correctly

--- integration-tests/js-compute/fixtures/request-upstream/bin/index.js
+++ integration-tests/js-compute/fixtures/request-upstream/bin/index.js
@@ -1,9 +1,14 @@
 /* eslint-env serviceworker */
 addEventListener("fetch", (event) => {
-  // Make a Request upstream to our origin
   let headers = new Headers();
   headers.set("UpstreamHeader", "UpstreamValue");
-  let upstreamRequest = new Request(
+
+  let upstreamRequest = new Request(event.request, {
+    headers,
+  });
+  
+  // Create a new Request object with an updated URL to our origin
+  upstreamRequest = new Request(
     "https://compute-sdk-test-backend.edgecompute.app/request_upstream",
     {
       headers,
