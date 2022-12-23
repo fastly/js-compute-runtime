290596e0596e34c31563bd6905614484c75c15b9
Guy Bedford <gbedford@fastly.com>
2022-12-24 01:27:19 +0200

    fixup test

--- integration-tests/cli/component.test.js
+++ integration-tests/cli/component.test.js
@@ -2,12 +2,13 @@ import test from 'brittle';
 import { getBinPath } from 'get-bin-path'
 import { prepareEnvironment } from '@jakechampion/cli-testing-library';
 import { print } from 'js-component-tools';
+import { resolve } from 'node:path';
 import { readFile } from 'node:fs/promises';
 
 const cli = await getBinPath()
 
 test('should create wasm file and return zero exit code', async function (t) {
-    const { execute, cleanup, writeFile, exists } = await prepareEnvironment();
+    const { execute, cleanup, path, writeFile, exists } = await prepareEnvironment();
     t.teardown(async function () {
         await cleanup();
     });
@@ -18,9 +19,14 @@ test('should create wasm file and return zero exit code', async function (t) {
 
     const { code, stdout, stderr } = await execute(process.execPath, cli + ' --component');
 
-    t.is(await exists('./bin/main.wasm'), true)
-    const source = print(await readFile('./bin/main.wasm'));
-    t.is(source.slice(0, 10), '(component');
+    t.is(await exists('./bin/main.wasm'), true);
+
+    // (necessary because readFile gives a string)
+    const wasmBuffer = await readFile(resolve(path, './bin/main.wasm'));
+
+    const wat = print(wasmBuffer);
+
+    t.is(wat.slice(0, 10), '(component');
     t.alike(stdout, []);
     t.alike(stderr, []);
     t.is(code, 0);
