334635a8f1235809482889afe4ba0cf31e559b71
Trevor Elliott <telliott@fastly.com>
2023-01-20 11:52:56 -0800

    Annotate cabi_realloc with its exported name

--- c-dependencies/js-compute-runtime/allocator.h
+++ c-dependencies/js-compute-runtime/allocator.h
@@ -14,7 +14,8 @@ extern "C" {
 
 /// A strong symbol to override the cabi_realloc defined by wit-bindgen. This version of
 /// cabi_realloc uses JS_malloc under the hood.
-void *cabi_realloc(void *ptr, size_t orig_size, size_t align, size_t new_size);
+__attribute__((export_name("cabi_realloc"))) void *cabi_realloc(void *ptr, size_t orig_size,
+                                                                size_t align, size_t new_size);
 
 /// A more ergonomic version of cabi_realloc for fresh allocations.
 inline void *cabi_malloc(size_t bytes, size_t align) { return cabi_realloc(NULL, 0, align, bytes); }
