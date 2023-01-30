a69d8b20d15100238583356fd81a65aaa32b2f66
Jake Champion <jchampion@fastly.com>
2023-01-30 13:21:18 +0000

    chore: add test to confirm that lots of the same header is supported

--- integration-tests/js-compute/fixtures/multiple-set-cookie/bin/index.js
+++ integration-tests/js-compute/fixtures/multiple-set-cookie/bin/index.js
@@ -37,6 +37,14 @@ routes.set('/', () => {
     h.append("Set-Cookie", "test=1; expires=Tue, 06-Dec-2022 12:34:56 GMT; Max-Age=60; Path=/; HttpOnly; Secure, test_id=1; Max-Age=60; Path=/; expires=Tue, 06-Dec-2022 12:34:56 GMT, test_id=1; Max-Age=60; Path=/; expires=Tue, 06-Dec-2022 12:34:56 GMT");
     h.append("Set-Cookie", "test2=2");
     h.append("Set-Cookie", "test3=3");
+    h.append("Set-Cookie", "test4=4");
+    h.append("Set-Cookie", "test5=5");
+    h.append("Set-Cookie", "test6=6");
+    h.append("Set-Cookie", "test7=7");
+    h.append("Set-Cookie", "test8=8");
+    h.append("Set-Cookie", "test9=9");
+    h.append("Set-Cookie", "test10=10");
+    h.append("Set-Cookie", "test11=11");
     
     let r =  new Response("Hello", {
       headers: h
@@ -49,6 +57,14 @@ routes.set('/', () => {
     r.headers.append("Set-Cookie", "test=1; expires=Tue, 06-Dec-2022 12:34:56 GMT; Max-Age=60; Path=/; HttpOnly; Secure, test_id=1; Max-Age=60; Path=/; expires=Tue, 06-Dec-2022 12:34:56 GMT, test_id=1; Max-Age=60; Path=/; expires=Tue, 06-Dec-2022 12:34:56 GMT");
     r.headers.append("Set-Cookie", "test2=2");
     r.headers.append("Set-Cookie", "test3=3");
+    r.headers.append("Set-Cookie", "test4=4");
+    r.headers.append("Set-Cookie", "test5=5");
+    r.headers.append("Set-Cookie", "test6=6");
+    r.headers.append("Set-Cookie", "test7=7");
+    r.headers.append("Set-Cookie", "test8=8");
+    r.headers.append("Set-Cookie", "test9=9");
+    r.headers.append("Set-Cookie", "test10=10");
+    r.headers.append("Set-Cookie", "test11=11");
     return r;
   });
 }
