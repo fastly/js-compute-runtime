193fb1f2d4cb8c9f7f886f2828e181781606b148
Guy Bedford <gbedford@fastly.com>
2023-01-30 16:22:22 -0800

    embed usage

--- src/component.js
+++ src/component.js
@@ -1,12 +1,10 @@
-import { componentNew } from 'js-component-tools';
+import { componentEmbed, componentNew } from '../../js-component-tools/dist/api.mjs';
 import { readFile, writeFile } from 'node:fs/promises';
 
 export async function compileComponent (path) {
   const coreComponent = await readFile(path);
   const wit = await readFile(new URL('../xqd.wit', import.meta.url), 'utf8');
-  const generatedComponent = await componentNew(coreComponent, {
-    adapters: [['wasi_snapshot_preview1', await readFile(new URL('../wasi_snapshot_preview1.wasm', import.meta.url))]],
-    wit
-  });
+  const coreComponentEmbedded = await componentEmbed(coreComponent, wit);
+  const generatedComponent = await componentNew(coreComponentEmbedded, [['wasi_snapshot_preview1', await readFile(new URL('../wasi_snapshot_preview1.wasm', import.meta.url))]]);
   await writeFile(path, generatedComponent);
 }
