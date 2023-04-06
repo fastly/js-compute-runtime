c0dc093c912ffa9380e615a64d75f0bc607d0134
Jake Champion <me@jakechampion.name>
2023-04-06 15:35:31 +0100

    Update tests/wpt-harness/pre-harness.js

--- tests/wpt-harness/pre-harness.js
+++ tests/wpt-harness/pre-harness.js
@@ -4,7 +4,6 @@ globalThis.GLOBAL = {
   isShadowRealm: function() { return false; },
 };
 
-function ReadableByteStreamController () {}
 
 globalThis.Window=self;
 let originalAEL = addEventListener;
