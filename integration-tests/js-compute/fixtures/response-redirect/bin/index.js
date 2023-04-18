8ad8ced540223f69de6c6e43b76c0723da73bd16
Jake Champion <jchampion@fastly.com>
2023-04-18 02:54:36 +0100

    feat: implement Response.redirect static method and Response.prototype.redirected getter

---
+++ integration-tests/js-compute/fixtures/response-redirect/bin/index.js
@@ -0,0 +1,44 @@
+/* eslint-env serviceworker */
+import { pass, assert, assertThrows } from "../../../assertions.js";
+import { routes } from "./test-harness.js";
+
+routes.set("/response/redirect", async () => {
+    const url = "http://test.url:1234/";
+    const redirectResponse = Response.redirect(url);
+    let error = assert(redirectResponse.type, "default");
+    if (error) { return error; }
+    error = assert(redirectResponse.redirected, false);
+    if (error) { return error; }
+    error = assert(redirectResponse.ok, false);
+    if (error) { return error; }
+    error = assert(redirectResponse.status, 302, "Default redirect status is 302");
+    if (error) { return error; }
+    error = assert(redirectResponse.headers.get("Location"), url)
+    if (error) { return error; }
+    error = assert(redirectResponse.statusText, "");
+    if (error) { return error; }
+
+    for (const status of [301, 302, 303, 307, 308]) {
+        const redirectResponse = Response.redirect(url, status);
+        error = assert(redirectResponse.type, "default");
+        if (error) { return error; }
+        error = assert(redirectResponse.redirected, false);
+        if (error) { return error; }
+        error = assert(redirectResponse.ok, false);
+        if (error) { return error; }
+        error = assert(redirectResponse.status, status, "Redirect status is " + status);
+        if (error) { return error; }
+        error = assert(redirectResponse.headers.get("Location"), url);
+        if (error) { return error; }
+        error = assert(redirectResponse.statusText, "");
+        if (error) { return error; }
+    }
+    const invalidUrl = "http://:This is not an url";
+    error = assertThrows(function () { Response.redirect(invalidUrl); }, TypeError);
+    if (error) { return error; }
+    for (const invalidStatus of [200, 309, 400, 500]) {
+        error = assertThrows(function () { Response.redirect(url, invalidStatus); }, RangeError);
+        if (error) { return error; }
+    }
+    return pass()
+})
---
+++ integration-tests/js-compute/fixtures/response-redirect/bin/test-harness.js
@@ -0,0 +1,32 @@
+/* eslint-env serviceworker */
+import { env } from 'fastly:env';
+import { fail } from "../../../assertions.js";
+
+addEventListener("fetch", event => {
+    event.respondWith(app(event))
+})
+/**
+ * @param {FetchEvent} event
+ * @returns {Response}
+ */
+async function app(event) {
+    try {
+        const path = (new URL(event.request.url)).pathname
+        console.log(`path: ${path}`)
+        console.log(`FASTLY_SERVICE_VERSION: ${env('FASTLY_SERVICE_VERSION')}`)
+        if (routes.has(path)) {
+            const routeHandler = routes.get(path)
+            return await routeHandler()
+        }
+        return fail(`${path} endpoint does not exist`)
+    } catch (error) {
+        return fail(`The routeHandler threw an error: ${error.message}` + '\n' + error.stack)
+    }
+}
+
+export const routes = new Map()
+routes.set('/', () => {
+    routes.delete('/')
+    let test_routes = Array.from(routes.keys())
+    return new Response(JSON.stringify(test_routes), { 'headers': { 'content-type': 'application/json' } })
+})
