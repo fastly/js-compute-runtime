7b5a5dc7e7ea707e0c9ccf8331de2c0cefa0c47a
Jake Champion <jchampion@fastly.com>
2023-04-23 00:47:31 +0100

    fix(TextDecoder/TextEncoder): Reimplement TextDecoder/Encoder using encoding_rs
    encoding_rs is a high-decode-performance, low-legacy-encode-footprint and high-correctness implementation of the WHATWG Encoding Standard written in Rust.
    
    This patch gets us

--- c-dependencies/js-compute-runtime/builtins/shared/text-decoder.cpp
+++ c-dependencies/js-compute-runtime/builtins/shared/text-decoder.cpp
@@ -1,48 +1,88 @@
 #include "builtins/shared/text-decoder.h"
 #include "builtin.h"
 #include "js-compute-builtins.h"
+#include "rust-encoding/rust-encoding.h"
 
 namespace builtins {
 
 bool TextDecoder::decode(JSContext *cx, unsigned argc, JS::Value *vp) {
-  METHOD_HEADER(1)
+  METHOD_HEADER(0);
 
-  // Default to empty string if no input is given.
-  if (args[0].isUndefined()) {
-    args.rval().set(JS_GetEmptyStringValue(cx));
-    return true;
+  auto source_value = args.get(0);
+  std::optional<std::span<uint8_t>> src;
+
+  if (source_value.isUndefined()) {
+    src = std::span<uint8_t, 0>();
+  } else {
+    src = value_to_buffer(cx, source_value, "TextDecoder#decode: input");
+  }
+  if (!src.has_value()) {
+    return false;
   }
 
-  auto data = value_to_buffer(cx, args[0], "TextDecoder#decode: input");
-  if (!data.has_value()) {
-    return false;
+  bool stream = false;
+  if (args.hasDefined(1)) {
+    auto options_value = args.get(1);
+    if (!options_value.isObject()) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                JSMSG_TEXT_DECODER_DECODE_OPTIONS_NOT_DICTIONARY);
+      return false;
+    }
+    JS::RootedObject options(cx, &options_value.toObject());
+    JS::RootedValue stream_value(cx);
+    if (!JS_GetProperty(cx, options, "stream", &stream_value)) {
+      return false;
+    }
+    stream = JS::ToBoolean(stream_value);
   }
 
   auto fatal =
       JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Fatal)).toBoolean();
   auto ignoreBOM =
       JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::IgnoreBOM)).toBoolean();
+  auto decoder = reinterpret_cast<jsencoding::Decoder *>(
+      JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Decoder)).toPrivate());
+  MOZ_ASSERT(decoder);
 
-  JS::RootedString str(cx);
-  if (ignoreBOM) {
-    auto chars = JS::UTF8Chars((char *)data->data(), data->size());
-    size_t length;
-    auto lossyUTF8Chars = JS::LossyUTF8CharsToNewTwoByteCharsZ(cx, chars, &length, js::MallocArena);
-    JS::UniqueTwoByteChars lossyChars(static_cast<char16_t *>(JS_malloc(cx, length + 1)));
-    if (!lossyChars) {
+  uint32_t result;
+  size_t srcLen = src->size();
+  size_t destLen = jsencoding::decoder_max_utf16_buffer_length(decoder, srcLen);
+  std::unique_ptr<uint16_t[]> dest(new uint16_t[destLen + 1]);
+  if (!dest) {
+    JS_ReportOutOfMemory(cx);
+    return false;
+  }
+  if (fatal) {
+    result = jsencoding::decoder_decode_to_utf16_without_replacement(decoder, src->data(), &srcLen,
+                                                                     dest.get(), &destLen, !stream);
+    if (result != 0) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_TEXT_DECODER_DECODING_FAILED);
       return false;
     }
-    memcpy(lossyChars.get(), lossyUTF8Chars.get(), length);
-
-    str.set(JS_NewUCStringDontDeflate(cx, std::move(lossyChars), length));
-
   } else {
-    str.set(JS_NewStringCopyUTF8N(cx, JS::UTF8Chars((char *)data->data(), data->size())));
+    bool hadReplacements;
+    result = jsencoding::decoder_decode_to_utf16(decoder, src->data(), &srcLen, dest.get(),
+                                                 &destLen, !stream, &hadReplacements);
   }
-  if (!str) {
-    if (fatal) {
-      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_TEXT_DECODER_DECODING_FAILED);
+  MOZ_ASSERT(result == 0);
+
+  auto encoding = reinterpret_cast<jsencoding::Encoding *>(
+      JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Encoding)).toPrivate());
+  MOZ_ASSERT(encoding);
+  // If the internal streaming flag of the decoder object is not set,
+  // then reset the encoding algorithm state to the default values
+  if (!stream) {
+    if (ignoreBOM) {
+      jsencoding::encoding_new_decoder_without_bom_handling_into(encoding, decoder);
+    } else {
+      jsencoding::encoding_new_decoder_with_bom_removal_into(encoding, decoder);
     }
+  }
+
+  JS::RootedString str(cx,
+                       JS_NewUCStringCopyN(cx, reinterpret_cast<char16_t *>(dest.get()), destLen));
+  if (!str) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_TEXT_DECODER_DECODING_FAILED);
     return false;
   }
 
@@ -51,13 +91,79 @@ bool TextDecoder::decode(JSContext *cx, unsigned argc, JS::Value *vp) {
 }
 
 bool TextDecoder::encoding_get(JSContext *cx, unsigned argc, JS::Value *vp) {
-  METHOD_HEADER(0)
+  METHOD_HEADER(0);
 
-  JS::RootedString str(cx, JS_NewStringCopyN(cx, "utf-8", 5));
-  if (!str)
+  JS::RootedObject result(cx);
+  if (!JS_GetPrototype(cx, self, &result)) {
     return false;
+  }
+  if (result != TextDecoder::proto_obj) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessageBuiltin, nullptr, JSMSG_INVALID_INTERFACE,
+                              "encoding get", "TextDecoder");
+    return false;
+  }
+
+  auto encoding = reinterpret_cast<jsencoding::Encoding *>(
+      JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Encoding)).toPrivate());
+  MOZ_ASSERT(encoding);
+
+  std::unique_ptr<uint8_t[]> name(new uint8_t[jsencoding::ENCODING_NAME_MAX_LENGTH]);
+  if (!name) {
+    JS_ReportOutOfMemory(cx);
+    return false;
+  }
+  size_t length = jsencoding::encoding_name(encoding, name.get());
+  // encoding_rs/jsencoding returns the name uppercase but we need to have it lowercased
+  for (size_t i = 0; i < length; i++) {
+    name[i] = std::tolower(name[i]);
+  }
+  JS::RootedString str(cx, JS_NewStringCopyN(cx, reinterpret_cast<char *>(name.get()), length));
+  if (!str) {
+    JS_ReportOutOfMemory(cx);
+    return false;
+  }
 
   args.rval().setString(str);
+  return true;
+}
+
+bool TextDecoder::fatal_get(JSContext *cx, unsigned argc, JS::Value *vp) {
+  METHOD_HEADER(0);
+
+  JS::RootedObject result(cx);
+  if (!JS_GetPrototype(cx, self, &result)) {
+    return false;
+  }
+  if (result != TextDecoder::proto_obj) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessageBuiltin, nullptr, JSMSG_INVALID_INTERFACE,
+                              "fatal get", "TextDecoder");
+    return false;
+  }
+
+  auto fatal =
+      JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Fatal)).toBoolean();
+
+  args.rval().setBoolean(fatal);
+  return true;
+}
+
+bool TextDecoder::ignoreBOM_get(JSContext *cx, unsigned argc, JS::Value *vp) {
+  METHOD_HEADER(0);
+
+  JS::RootedObject result(cx);
+  if (!JS_GetPrototype(cx, self, &result)) {
+    return false;
+  }
+  if (result != TextDecoder::proto_obj) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessageBuiltin, nullptr, JSMSG_INVALID_INTERFACE,
+                              "ignoreBOM get", "TextDecoder");
+    return false;
+  }
+
+  auto ignoreBOM =
+      JS::GetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::IgnoreBOM)).toBoolean();
+
+  args.rval().setBoolean(ignoreBOM);
   return true;
 }
 
@@ -70,12 +176,14 @@ const JSPropertySpec TextDecoder::static_properties[] = {
 };
 
 const JSFunctionSpec TextDecoder::methods[] = {
-    JS_FN("decode", decode, 1, JSPROP_ENUMERATE),
+    JS_FN("decode", decode, 0, JSPROP_ENUMERATE),
     JS_FS_END,
 };
 
 const JSPropertySpec TextDecoder::properties[] = {
     JS_PSG("encoding", encoding_get, JSPROP_ENUMERATE),
+    JS_PSG("fatal", fatal_get, JSPROP_ENUMERATE),
+    JS_PSG("ignoreBOM", ignoreBOM_get, JSPROP_ENUMERATE),
     JS_STRING_SYM_PS(toStringTag, "TextDecoder", JSPROP_READONLY),
     JS_PS_END,
 };
@@ -83,60 +191,63 @@ const JSPropertySpec TextDecoder::properties[] = {
 // constructor(optional DOMString label = "utf-8", optional TextDecoderOptions options = {});
 bool TextDecoder::constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
   CTOR_HEADER("TextDecoder", 0);
-
   // 1. Let encoding be the result of getting an encoding from label.
   auto label_value = args.get(0);
-  JS::RootedValue encoding(cx);
-  if (!label_value.isUndefined()) {
-    // https://encoding.spec.whatwg.org/#concept-encoding-get
-    // To get an encoding from a string label, run these steps:
-    size_t length;
+  // https://encoding.spec.whatwg.org/#concept-encoding-get
+  // To get an encoding from a string label, run these steps:
+  // 1. Remove any leading and trailing ASCII whitespace from label.
+  // 2. If label is an ASCII case-insensitive match for any of the labels listed in the table
+  // below, then return the corresponding encoding; otherwise return failure. JS-Compute-Runtime:
+  size_t length;
+  jsencoding::Encoding *encoding;
+  if (label_value.isUndefined()) {
+    encoding = const_cast<jsencoding::Encoding *>(jsencoding::encoding_for_label_no_replacement(
+        reinterpret_cast<uint8_t *>(const_cast<char *>("UTF-8")), 5));
+  } else {
     auto label_chars = encode(cx, label_value, &length);
     if (!label_chars) {
       return false;
     }
-    std::string label(label_chars.get(), length);
-    // 1. Remove any leading and trailing ASCII whitespace from label.
-    auto ascii_whitespace = "\t\n\f\r ";
-    label.erase(0, label.find_first_not_of(ascii_whitespace)); // left trim
-    label.erase(label.find_last_not_of(ascii_whitespace) + 1); // right trim
-    // 2. If label is an ASCII case-insensitive match for any of the labels listed in the table
-    // below, then return the corresponding encoding; otherwise return failure. JS-Compute-Runtime:
-    // We only support utf-8 right now so we only allow that label through.
-    std::transform(label.begin(), label.end(), label.begin(),
-                   [](unsigned char c) { return std::tolower(c); });
-    if (label == "unicode-1-1-utf-8" || label == "unicode11utf8" || label == "unicode20utf8" ||
-        label == "utf-8" || label == "utf8" || label == "x-unicode20utf8") {
-      encoding.setString(JS_NewStringCopyZ(cx, "UTF-8"));
-    } else {
-      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_TEXT_DECODER_INVALID_ENCODING);
-      return false;
-    }
-  } else {
-    encoding.setString(JS_NewStringCopyZ(cx, "UTF-8"));
+    encoding = const_cast<jsencoding::Encoding *>(jsencoding::encoding_for_label_no_replacement(
+        reinterpret_cast<uint8_t *>(label_chars.get()), length));
   }
-  auto options_val = args.get(1);
-  bool fatal = false;
-  bool ignoreBOM = false;
-  if (options_val.isObject()) {
-    JS::RootedObject options(cx, &options_val.toObject());
-    JS::RootedValue fatal_value(cx);
-    if (!JS_GetProperty(cx, options, "fatal", &fatal_value)) {
-      return false;
-    }
-    fatal = fatal_value.toBoolean();
-    JS::RootedValue ignoreBOM_value(cx);
-    if (!JS_GetProperty(cx, options, "ignoreBOM", &ignoreBOM_value)) {
-      return false;
-    }
-    ignoreBOM = ignoreBOM_value.toBoolean();
-  } else if (!options_val.isNullOrUndefined()) {
-    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                              JSMSG_TEXT_DECODER_OPTIONS_NOT_DICTIONARY);
+  if (!encoding) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr, JSMSG_TEXT_DECODER_INVALID_ENCODING);
     return false;
   }
+  bool fatal = false;
+  bool ignoreBOM = false;
+  if (args.hasDefined(1)) {
+    auto options_val = args.get(1);
+    if (options_val.isObject()) {
+      JS::RootedObject options(cx, &options_val.toObject());
+      JS::RootedValue fatal_value(cx);
+      if (!JS_GetProperty(cx, options, "fatal", &fatal_value)) {
+        return false;
+      }
+      fatal = JS::ToBoolean(fatal_value);
+      JS::RootedValue ignoreBOM_value(cx);
+      if (!JS_GetProperty(cx, options, "ignoreBOM", &ignoreBOM_value)) {
+        return false;
+      }
+      ignoreBOM = JS::ToBoolean(ignoreBOM_value);
+    } else if (!options_val.isNull()) {
+      JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                JSMSG_TEXT_DECODER_OPTIONS_NOT_DICTIONARY);
+      return false;
+    }
+  }
   JS::RootedObject self(cx, JS_NewObjectForConstructor(cx, &class_, args));
-  JS::SetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Encoding), encoding);
+  jsencoding::Decoder *decoder;
+  if (ignoreBOM) {
+    decoder = jsencoding::encoding_new_decoder_without_bom_handling(encoding);
+  } else {
+    decoder = jsencoding::encoding_new_decoder_with_bom_removal(encoding);
+  }
+  JS::SetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Decoder),
+                      JS::PrivateValue(decoder));
+  JS::SetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Encoding),
+                      JS::PrivateValue(encoding));
   JS::SetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::Fatal),
                       JS::BooleanValue(fatal));
   JS::SetReservedSlot(self, static_cast<uint32_t>(TextDecoder::Slots::IgnoreBOM),
--- c-dependencies/js-compute-runtime/builtins/shared/text-encoder.cpp
+++ c-dependencies/js-compute-runtime/builtins/shared/text-encoder.cpp
@@ -1,7 +1,10 @@
 #include "builtins/shared/text-encoder.h"
+#include "js-compute-builtins.h"
+#include <iostream>
+#include <tuple>
 
 #include "js/ArrayBuffer.h"
-
+#include "mozilla/Span.h"
 // TODO: remove these once the warnings are fixed
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Winvalid-offsetof"
@@ -11,13 +14,14 @@
 namespace builtins {
 
 bool TextEncoder::encode(JSContext *cx, unsigned argc, JS::Value *vp) {
-  METHOD_HEADER(1)
+  METHOD_HEADER(0);
 
   // Default to empty string if no input is given.
   if (args.get(0).isUndefined()) {
     JS::RootedObject byte_array(cx, JS_NewUint8Array(cx, 0));
-    if (!byte_array)
+    if (!byte_array) {
       return false;
+    }
 
     args.rval().setObject(*byte_array);
     return true;
@@ -34,19 +38,92 @@ bool TextEncoder::encode(JSContext *cx, unsigned argc, JS::Value *vp) {
   }
 
   JS::RootedObject byte_array(cx, JS_NewUint8ArrayWithBuffer(cx, buffer, 0, chars_len));
-  if (!byte_array)
+  if (!byte_array) {
     return false;
+  }
 
   args.rval().setObject(*byte_array);
   return true;
 }
 
+bool TextEncoder::encodeInto(JSContext *cx, unsigned argc, JS::Value *vp) {
+  METHOD_HEADER(2);
+
+  auto source = JS::ToString(cx, args.get(0));
+  if (!source) {
+    return false;
+  }
+  auto destination_value = args.get(1);
+
+  if (!destination_value.isObject()) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                              JSMSG_TEXT_ENCODER_ENCODEINTO_INVALID_ARRAY);
+    return false;
+  }
+  JS::RootedObject destination(cx, &destination_value.toObject());
+  if (!JS_IsArrayBufferViewObject(destination)) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                              JSMSG_TEXT_ENCODER_ENCODEINTO_INVALID_ARRAY);
+    return false;
+  }
+  if (JS::IsLargeArrayBufferView(destination)) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                              JSMSG_TEXT_ENCODER_ENCODEINTO_INVALID_ARRAY);
+    return false;
+  }
+
+  uint8_t *data;
+  bool is_shared;
+  size_t len = 0;
+  if (!JS_GetObjectAsUint8Array(destination, &len, &is_shared, &data)) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                              JSMSG_TEXT_ENCODER_ENCODEINTO_INVALID_ARRAY);
+    return false;
+  }
+  auto span = AsWritableChars(mozilla::Span(data, len));
+  auto maybe = JS_EncodeStringToUTF8BufferPartial(cx, source, span);
+  if (!maybe) {
+    return false;
+  }
+  size_t read = Get<0>(*maybe);
+  size_t written = Get<1>(*maybe);
+
+  MOZ_ASSERT(written <= len);
+
+  JS::RootedObject obj(cx, JS_NewPlainObject(cx));
+  if (!obj) {
+    return false;
+  }
+  JS::RootedValue read_value(cx, JS::NumberValue(read));
+  JS::RootedValue written_value(cx, JS::NumberValue(written));
+  if (!JS_SetProperty(cx, obj, "read", read_value)) {
+    return false;
+  }
+  if (!JS_SetProperty(cx, obj, "written", written_value)) {
+    return false;
+  }
+
+  args.rval().setObject(*obj);
+  return true;
+}
+
 bool TextEncoder::encoding_get(JSContext *cx, unsigned argc, JS::Value *vp) {
-  METHOD_HEADER(0)
+  METHOD_HEADER(0);
+
+  JS::RootedObject result(cx);
+  if (!JS_GetPrototype(cx, self, &result)) {
+    return false;
+  }
+  if (result != TextEncoder::proto_obj) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessageBuiltin, nullptr, JSMSG_INVALID_INTERFACE,
+                              "encoding get", "TextEncoder");
+    return false;
+  }
 
   JS::RootedString str(cx, JS_NewStringCopyN(cx, "utf-8", 5));
-  if (!str)
+  if (!str) {
     return false;
+  }
 
   args.rval().setString(str);
   return true;
@@ -61,7 +138,8 @@ const JSPropertySpec TextEncoder::static_properties[] = {
 };
 
 const JSFunctionSpec TextEncoder::methods[] = {
-    JS_FN("encode", encode, 1, JSPROP_ENUMERATE),
+    JS_FN("encode", encode, 0, JSPROP_ENUMERATE),
+    JS_FN("encodeInto", encodeInto, 2, JSPROP_ENUMERATE),
     JS_FS_END,
 };
 
@@ -75,8 +153,9 @@ bool TextEncoder::constructor(JSContext *cx, unsigned argc, JS::Value *vp) {
   CTOR_HEADER("TextEncoder", 0);
 
   JS::RootedObject self(cx, JS_NewObjectForConstructor(cx, &class_, args));
-  if (!self)
+  if (!self) {
     return false;
+  }
 
   args.rval().setObject(*self);
   return true;
