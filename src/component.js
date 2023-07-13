2d703e3197d1b072f3c754f2864598b29de04bf8
Trevor Elliott <telliott@fastly.com>
2023-07-13 12:44:17 -0700

    Compile the type information from wit-bindgen into components directly

--- src/component.js
+++ src/component.js
@@ -1,13 +1,8 @@
-import { componentEmbed, componentNew, preview1AdapterReactorPath } from '@bytecodealliance/jco';
+import { componentNew, preview1AdapterReactorPath } from '@bytecodealliance/jco';
 import { readFile, writeFile } from 'node:fs/promises';
 
 export async function compileComponent (path) {
   const coreComponent = await readFile(path);
-  const wit = await readFile(new URL('../fastly.wit', import.meta.url), 'utf8');
-  const coreComponentEmbedded = await componentEmbed({
-    binary: coreComponent,
-    witSource: wit,
-  });
-  const generatedComponent = await componentNew(coreComponentEmbedded, [['wasi_snapshot_preview1', await readFile(preview1AdapterReactorPath())]]);
+  const generatedComponent = await componentNew(coreComponent, [['wasi_snapshot_preview1', await readFile(preview1AdapterReactorPath())]]);
   await writeFile(path, generatedComponent);
 }
