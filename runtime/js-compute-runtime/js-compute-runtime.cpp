a9d09ac0cab592da7f90b3c5d6f51ae0eb537c25
Jake Champion <jchampion@fastly.com>
2023-08-08 11:09:59 +0100

    compile gecko-dev with -O3 to force more functions to get inlined

--- runtime/js-compute-runtime/js-compute-runtime.cpp
+++ runtime/js-compute-runtime/js-compute-runtime.cpp
@@ -125,8 +125,10 @@ bool init_js() {
 
   bool ENABLE_PBL = std::string(std::getenv("ENABLE_PBL")) == "1";
   if (ENABLE_PBL) {
-    JS_SetGlobalJitCompilerOption(cx, JSJitCompilerOption::JSJITCOMPILER_PORTABLE_BASELINE_ENABLE, 1);
-    JS_SetGlobalJitCompilerOption(cx, JSJitCompilerOption::JSJITCOMPILER_PORTABLE_BASELINE_WARMUP_THRESHOLD, 0);
+    JS_SetGlobalJitCompilerOption(cx, JSJitCompilerOption::JSJITCOMPILER_PORTABLE_BASELINE_ENABLE,
+                                  1);
+    JS_SetGlobalJitCompilerOption(
+        cx, JSJitCompilerOption::JSJITCOMPILER_PORTABLE_BASELINE_WARMUP_THRESHOLD, 0);
   }
 
   // TODO: check if we should set a different creation zone.
