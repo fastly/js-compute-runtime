8c3a63b0be63665235df39929eb360fa12264f6d
Jake Champion <jchampion@fastly.com>
2023-08-23 00:06:43 +0100

    use existing config store or create a new one

---
+++ integration-tests/js-compute/fixtures/edge-dictionary/setup.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import { $ as zx } from 'zx'
+
+const startTime = Date.now();
+
+zx.verbose = false;
+if (process.env.FASTLY_API_TOKEN === undefined) {
+    try {
+        process.env.FASTLY_API_TOKEN = String(await zx`fastly profile token --quiet`).trim()
+    } catch {
+        console.error('No environment variable named FASTLY_API_TOKEN has been set and no default fastly profile exists.');
+        console.error('In order to run the tests, either create a fastly profile using `fastly profile create` or export a fastly token under the name FASTLY_API_TOKEN');
+        process.exit(1)
+    }
+}
+
+zx.verbose = true;
+
+let stores = await (async function() {
+    try {
+        return JSON.parse(await zx`fastly config-store list --quiet --json --token $FASTLY_API_TOKEN`)
+    } catch {
+        return {data:[]}
+    }
+}())
+
+const STORE_ID = stores.data?.find(({ name }) => name === 'aZ1 __ 2')?.id
+if (!STORE_ID) {
+    process.env.STORE_ID = JSON.parse(await zx`fastly config-store create --quiet --name='aZ1 __ 2' --json --token $FASTLY_API_TOKEN`).id
+} else {
+    process.env.STORE_ID = STORE_ID;
+}
+
+try {
+    await zx`echo -n 'https://twitter.com/fastly' | fastly config-store-entry create --name twitter --store-id=$STORE_ID --stdin --token $FASTLY_API_TOKEN`
+} catch {}
+
+await zx`fastly resource-link create --version latest --resource-id $STORE_ID --token $FASTLY_API_TOKEN --autoclone`
+await zx`fastly service-version activate --version latest --token $FASTLY_API_TOKEN`
+
+console.log(`Set up has finished! Took ${(Date.now() - startTime) / 1000} seconds to complete`);
---
+++ integration-tests/js-compute/fixtures/edge-dictionary/teardown.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+import { $ as zx } from 'zx'
+
+const startTime = Date.now();
+
+
+zx.verbose = false;
+if (process.env.FASTLY_API_TOKEN === undefined) {
+    try {
+        process.env.FASTLY_API_TOKEN = String(await zx`fastly profile token --quiet`).trim()
+    } catch {
+        console.error('No environment variable named FASTLY_API_TOKEN has been set and no default fastly profile exists.');
+        console.error('In order to run the tests, either create a fastly profile using `fastly profile create` or export a fastly token under the name FASTLY_API_TOKEN');
+        process.exit(1)
+    }
+}
+zx.verbose = true;
+let stores = await (async function() {
+    try {
+        return JSON.parse(await zx`fastly config-store list --quiet --json --token $FASTLY_API_TOKEN`)
+    } catch {
+        return {data:[]}
+    }
+}())
+let links = await (async function() {
+    try {
+        return JSON.parse(await zx`fastly resource-link list --quiet --json --version latest --token $FASTLY_API_TOKEN`)
+    } catch {
+        return []
+    }
+}())
+
+const STORE_ID = stores.data.find(({ name }) => name === 'aZ1 __ 2')?.id
+if (STORE_ID) {
+    process.env.STORE_ID = STORE_ID;
+    let LINK_ID = links.find(({resource_id}) => resource_id == STORE_ID)?.id;
+    if (LINK_ID) {
+        process.env.LINK_ID = LINK_ID;
+        try {
+            await zx`fastly resource-link delete --version latest --autoclone --id=$LINK_ID  --token $FASTLY_API_TOKEN`
+            await zx`fastly service-version activate --version latest --token $FASTLY_API_TOKEN`
+        } catch {}
+    }
+    try {
+        await zx`fastly config-store delete --store-id=$STORE_ID  --token $FASTLY_API_TOKEN`
+    } catch {}
+}
+
+console.log(`Tear down has finished! Took ${(Date.now() - startTime) / 1000} seconds to complete`);
