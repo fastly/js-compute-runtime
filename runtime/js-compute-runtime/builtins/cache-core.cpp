a7ce4ce7de94851a2f1f4ad23b16e411de63e4fc
Jake Champion <jchampion@fastly.com>
2024-02-23 09:38:44 +0000

    fix formatting

--- runtime/js-compute-runtime/builtins/cache-core.cpp
+++ runtime/js-compute-runtime/builtins/cache-core.cpp
@@ -48,9 +48,10 @@ JS::Result<host_api::CacheLookupOptions> parseLookupOptions(JSContext *cx,
         return JS::Result<host_api::CacheLookupOptions>(JS::Error());
       }
 
-      // We need to convert the supplied HeadersInit in the `headers` property into a host-backed Request which contains the same headers
-      // builtins::Request::create does exactly that however, it also expects a fully valid URL for the Request.
-      // We don't ever use the Request URL, so we hard-code a valid URL
+      // We need to convert the supplied HeadersInit in the `headers` property into a host-backed
+      // Request which contains the same headers builtins::Request::create does exactly that
+      // however, it also expects a fully valid URL for the Request. We don't ever use the Request
+      // URL, so we hard-code a valid URL
       JS::RootedValue input(cx, JS::StringValue(JS_NewStringCopyZ(cx, "http://example.com")));
       JS::RootedObject request(cx,
                                builtins::Request::create(cx, requestInstance, input, headers_val));
@@ -332,9 +333,10 @@ JS::Result<host_api::CacheWriteOptions> parseInsertOptions(JSContext *cx,
       return JS::Result<host_api::CacheWriteOptions>(JS::Error());
     }
 
-    // We need to convert the supplied HeadersInit in the `headers` property into a host-backed Request which contains the same headers
-      // builtins::Request::create does exactly that however, it also expects a fully valid URL for the Request.
-      // We don't ever use the Request URL, so we hard-code a valid URL
+    // We need to convert the supplied HeadersInit in the `headers` property into a host-backed
+    // Request which contains the same headers builtins::Request::create does exactly that however,
+    // it also expects a fully valid URL for the Request. We don't ever use the Request URL, so we
+    // hard-code a valid URL
     JS::RootedValue input(cx, JS::StringValue(JS_NewStringCopyZ(cx, "http://example.com")));
     JS::RootedObject request(cx,
                              builtins::Request::create(cx, requestInstance, input, headers_val));
@@ -1117,9 +1119,10 @@ bool CoreCache::transactionLookup(JSContext *cx, unsigned argc, JS::Value *vp) {
         return false;
       }
 
-      // We need to convert the supplied HeadersInit in the `headers` property into a host-backed Request which contains the same headers
-      // builtins::Request::create does exactly that however, it also expects a fully valid URL for the Request.
-      // We don't ever use the Request URL, so we hard-code a valid URL
+      // We need to convert the supplied HeadersInit in the `headers` property into a host-backed
+      // Request which contains the same headers builtins::Request::create does exactly that
+      // however, it also expects a fully valid URL for the Request. We don't ever use the Request
+      // URL, so we hard-code a valid URL
       JS::RootedValue input(cx, JS::StringValue(JS_NewStringCopyZ(cx, "http://example.com")));
       JS::RootedObject request(cx,
                                builtins::Request::create(cx, requestInstance, input, headers_val));
