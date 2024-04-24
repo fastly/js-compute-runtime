cc2a758794988b99803d9dd52cd14d8283cf97e3
Guy Bedford <gbedford@fastly.com>
2024-04-23 19:24:49 -0700

    final fixup for now

--- integration-tests/js-compute/fixtures/app/src/include-bytes.js
+++ integration-tests/js-compute/fixtures/app/src/include-bytes.js
@@ -1,18 +1,17 @@
 import { routes } from "./routes.js";
-import { assert, pass, fail } from "./assertions.js";
+import { assert, pass } from "./assertions.js";
 import { includeBytes } from "fastly:experimental";
 
-let message, err
+let message;
 try {
   message = includeBytes("message.txt");
-} catch (e) {
-  err = e;
-}
+} catch {}
+
+const expected = [
+  104,101,108,108,111,32,105,110,99,108,117,100,101,66,121,116,101,115,10
+];
 
 routes.set("/includeBytes", () => {
-  if (err) {
-    return fail(err);
-  }
   let error = assert(Array.from(message), expected, `message === expected`);
   if (error) { return error; }
   return pass();
