ef6167effa8067422f0366ed84f7b843d483362f
Guy Bedford <gbedford@fastly.com>
2024-06-26 14:30:52 -0700

    fixup cli tests

---
+++ integration-tests/cli/starlingmonkey.test.js
@@ -0,0 +1,23 @@
+import test from 'brittle';
+import { getBinPath } from 'get-bin-path'
+import { prepareEnvironment } from '@jakechampion/cli-testing-library';
+
+const cli = await getBinPath()
+
+test('should create wasm file and return zero exit code for StarlingMonkey', async function (t) {
+    const { execute, cleanup, writeFile, exists } = await prepareEnvironment();
+    t.teardown(async function () {
+        await cleanup();
+    });
+
+    await writeFile('./bin/index.js', `addEventListener('fetch', function(){})`)
+    
+    t.is(await exists('./bin/main.wasm'), false)
+
+    const { code, stdout, stderr } = await execute(process.execPath, cli + ' --starlingmonkey');
+
+    t.is(await exists('./bin/main.wasm'), true)
+    t.alike(stdout, ['Building with the experimental StarlingMonkey engine']);
+    t.alike(stderr, []);
+    t.is(code, 0);
+});
