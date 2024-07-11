6035576b64c12b62e0d9978bb6aae29a7766183b
Guy Bedford <gbedford@fastly.com>
2024-07-11 14:16:22 -0700

    fix: fix documentation site deployment

--- documentation/app/src/index.js
+++ documentation/app/src/index.js
@@ -3,6 +3,10 @@
 
 import { get } from "@jakechampion/c-at-e-file-server";
 import { env } from "fastly:env";
+import { KVStore } from "fastly:kv-store";
+
+// Pending update to https://github.com/JakeChampion/compute-file-server/commit/58bdcd4a6234bba5b74502f5f03bcaffabc7ad02
+globalThis.ObjectStore = KVStore;
 
 addEventListener("fetch", (event) => event.respondWith(app(event)));
 
