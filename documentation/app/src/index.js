2508ea820e0a6d644b1a800f5b73f7ad42321e16
Jake Champion <jchampion@fastly.com>
2024-07-12 16:54:24 +0100

    move documentation website over to using @fastly/compute-js-static-publish
    @fastly/compute-js-static-publish is maintained by the dev-rel team, whereas what we used before was not maintained by anyone

--- documentation/app/src/index.js
+++ documentation/app/src/index.js
@@ -1,27 +1,21 @@
 /// <reference types="@fastly/js-compute" />
-/* eslint-env serviceworker */
+import { getServer } from '../static-publisher/statics.js';
+const staticContentServer = getServer();
 
-import { get } from "@jakechampion/c-at-e-file-server";
-import { env } from "fastly:env";
-import { KVStore } from "fastly:kv-store";
-
-// Pending update to https://github.com/JakeChampion/compute-file-server/commit/58bdcd4a6234bba5b74502f5f03bcaffabc7ad02
-globalThis.ObjectStore = KVStore;
-
+// eslint-disable-next-line no-undef
 addEventListener("fetch", (event) => event.respondWith(app(event)));
 
 async function app(event) {
-    try {
-        console.log(`FASTLY_SERVICE_VERSION: ${env('FASTLY_SERVICE_VERSION')}`)
-        const response = await get('site', event.request)
-        if (response) {
-            response.headers.set("x-compress-hint", "on");
-            return response
-        } else {
-            return new Response("Not Found", { status: 404 });
-        }
-    } catch (error) {
-        console.error(error);
-        return new Response(error.message + '\n' + error.stack, { status: 500 })
+  try {
+    const response = await staticContentServer.serveRequest(event.request);
+    if (response) {
+      response.headers.set("x-compress-hint", "on");
+      return response
+    } else {
+      return new Response("Not Found", { status: 404 });
     }
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message + '\n' + error.stack, { status: 500 })
+  }
 }
