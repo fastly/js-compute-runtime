540fbb09e08761072edaff243cde37a3ad6b9caf
Guy Bedford <gbedford@fastly.com>
2024-07-26 11:28:49 -0700

    update versions

--- runtime/fastly/build-release.sh
+++ runtime/fastly/build-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit 1
 RUNTIME_VERSION=$(npm pkg get version --json --prefix=../../ | jq -r)
-HOST_API=$(realpath host-api) cmake -B build-release -DCMAKE_BUILD_TYPE=Release -DENABLE_BUILTIN_WEB_FETCH=0 -DENABLE_BUILTIN_WEB_FETCH_FETCH_EVENT=0 -DRUNTIME_VERSION="\"starlingmonkey-$RUNTIME_VERSION\""
+HOST_API=$(realpath host-api) cmake -B build-release -DCMAKE_BUILD_TYPE=Release -DENABLE_BUILTIN_WEB_FETCH=0 -DENABLE_BUILTIN_WEB_FETCH_FETCH_EVENT=0 -DRUNTIME_VERSION="\"$RUNTIME_VERSION\""
 cmake --build build-release
 mv build-release/starling.wasm/starling.wasm ../../
