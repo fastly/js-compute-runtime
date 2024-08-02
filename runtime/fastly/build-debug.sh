211fad71db86e04556d95793fd8d593270879e8e
Jake Champion <me@jakechampion.name>
2024-08-02 09:19:40 +0100

    Update build-debug.sh

--- runtime/fastly/build-debug.sh
+++ runtime/fastly/build-debug.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
 cd "$(dirname "$0")" || exit 1
 RUNTIME_VERSION=$(npm pkg get version --json --prefix=../../ | jq -r)
 HOST_API=$(realpath host-api) cmake -B build-debug -DCMAKE_BUILD_TYPE=Debug -DENABLE_BUILTIN_WEB_FETCH=0 -DENABLE_BUILTIN_WEB_FETCH_FETCH_EVENT=0 -DRUNTIME_VERSION="\"$RUNTIME_VERSION-debug\""
