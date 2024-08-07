3b14fbbe72d76622c6fb64d955afc0e06ca36d5f
Guy Bedford <gbedford@fastly.com>
2024-08-07 14:45:49 -0700

    fixup

--- runtime/fastly/builtins/cache-core.cpp
+++ runtime/fastly/builtins/cache-core.cpp
@@ -333,13 +333,14 @@ JS::Result<host_api::CacheWriteOptions> parseInsertOptions(JSContext *cx,
     if (!requestInstance) {
       return JS::Result<host_api::CacheWriteOptions>(JS::Error());
     }
+    JS::RootedValue request_opts_val(cx, ObjectValue(*request_opts));
+
     // We need to convert the supplied HeadersInit in the `headers` property into a host-backed
     // Request which contains the same headers Request::create does exactly that however,
     // it also expects a fully valid URL for the Request. We don't ever use the Request URL, so we
     // hard-code a valid URL
     JS::RootedValue input(cx, JS::StringValue(JS_NewStringCopyZ(cx, "http://example.com")));
-    JS::RootedObject request(
-        cx, Request::create(cx, requestInstance, input, JS::ObjectValue(*request_opts)));
+    JS::RootedObject request(cx, Request::create(cx, requestInstance, input, request_opts_val));
     options.request_headers = host_api::HttpReq(Request::request_handle(request));
   }
   return options;
