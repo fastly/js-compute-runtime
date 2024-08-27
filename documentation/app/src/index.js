ba1eb667cb136c6cdd09938e397ee5cf334561d7
Guy Bedford <gbedford@fastly.com>
2024-08-27 05:27:53 +0200

    fix: revert documentation website refactoring (#915)

--- documentation/app/src/index.js
+++ documentation/app/src/index.js
@@ -1,21 +1,27 @@
 /// <reference types="@fastly/js-compute" />
-import { getServer } from '../static-publisher/statics.js';
-const staticContentServer = getServer();
+/* eslint-env serviceworker */
 
-// eslint-disable-next-line no-undef
+import { get } from "@jakechampion/c-at-e-file-server";
+import { env } from "fastly:env";
+import { KVStore } from "fastly:kv-store";
+
+// Pending update to https://github.com/JakeChampion/compute-file-server/commit/58bdcd4a6234bba5b74502f5f03bcaffabc7ad02
+globalThis.ObjectStore = KVStore;
+
 addEventListener("fetch", (event) => event.respondWith(app(event)));
 
 async function app(event) {
-  try {
-    const response = await staticContentServer.serveRequest(event.request);
-    if (response) {
-      response.headers.set("x-compress-hint", "on");
-      return response
-    } else {
-      return new Response("Not Found", { status: 404 });
+    try {
+        console.log(`FASTLY_SERVICE_VERSION: ${env('FASTLY_SERVICE_VERSION')}`)
+        const response = await get('site', event.request)
+        if (response) {
+            response.headers.set("x-compress-hint", "on");
+            return response
+        } else {
+            return new Response("Not Found", { status: 404 });
+        }
+    } catch (error) {
+        console.error(error);
+        return new Response(error.message + '\n' + error.stack, { status: 500 })
     }
-  } catch (error) {
-    console.error(error);
-    return new Response(error.message + '\n' + error.stack, { status: 500 })
-  }
 }
