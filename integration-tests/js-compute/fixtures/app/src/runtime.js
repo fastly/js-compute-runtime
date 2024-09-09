a55bb782872f9d9a8c2750aa67bf4ade40e0e538
Guy Bedford <gbedford@fastly.com>
2024-09-09 11:36:56 -0700

    generous range

--- integration-tests/js-compute/fixtures/app/src/runtime.js
+++ integration-tests/js-compute/fixtures/app/src/runtime.js
@@ -10,14 +10,14 @@ routes.set("/runtime/get-vcpu-ms", () => {
   const cpuTime = vCpuTime();
   strictEqual(typeof cpuTime, 'number');
   ok(cpuTime > 0);
-  ok(cpuTime < 1000);
+  ok(cpuTime < 3000);
   const arr = [];
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 3000; i++) {
     arr.push(i);
   }
   const cpuTime2 = vCpuTime();
   ok(cpuTime2 > cpuTime);
   ok(cpuTime2 - cpuTime > 1);
-  ok(cpuTime2 - cpuTime < 1000);
+  ok(cpuTime2 - cpuTime < 3000);
   return pass('ok');
 });
