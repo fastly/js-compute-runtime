af2d6d8996b692e7ba475d807732d74b404b0fd5
Guy Bedford <gbedford@fastly.com>
2025-03-11 19:23:48 -0700

    fixup

--- runtime/fastly/builtins/fetch/request-response.cpp
+++ runtime/fastly/builtins/fetch/request-response.cpp
@@ -835,7 +835,7 @@ bool RequestOrResponse::extract_body(JSContext *cx, JS::HandleObject self,
     }
 
     auto boundary = MultipartFormData::boundary(encoder);
-    
+
     host_type_str = host_api::HostString("multipart/form-data; boundary=" + boundary);
 
     auto length = MultipartFormData::query_length(cx, encoder);
