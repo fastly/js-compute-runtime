bd22c1d8f414ff9ddcb2c60456e611b3ccfe9bb5
vcarvajal-sigsci <vcarvajal@fastly.com>
2025-04-02 12:57:40 -0700

    fix: TypeScript declaration for Headers (#1155)

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1919,6 +1919,7 @@ interface Headers {
   append(name: string, value: string): void;
   delete(name: string): void;
   get(name: string): string | null;
+  getSetCookie() : string[];
   has(name: string): boolean;
   set(name: string, value: string): void;
   forEach(
