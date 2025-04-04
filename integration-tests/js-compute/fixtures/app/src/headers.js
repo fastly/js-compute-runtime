c3bd8e79f5a2910099dada26073b6162900d6956
vcarvajal-sigsci <vcarvajal@fastly.com>
2025-04-04 12:53:09 -0700

    add test for getSetCookie() header

--- integration-tests/js-compute/fixtures/app/src/headers.js
+++ integration-tests/js-compute/fixtures/app/src/headers.js
@@ -21,6 +21,14 @@ routes.set('/headers/non-ascii-latin1-field-value', async () => {
   assert(text, 'é', `response.headers.get('cat') === "é"`);
 });
 
+routes.set('/headers/getsetcookie', async () => {
+  let response = await fetch('https://http-me.glitch.me/meow?header=Set-Cookie:name1=value1', {
+    backend: 'httpme',
+  });
+  response.headers.append("Set-Cookie", "name2=value2");
+  console.log("response.headers.getSetCookie()", response.headers.getSetCookie());
+});
+
 routes.set('/headers/from-response/set', async () => {
   const response = await fetch('https://httpbin.org/stream-bytes/11', {
     backend: 'httpbin',
