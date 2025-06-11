477a699bc76afe16cbe6696c4b326b1c08fd1e74
Charles Eckman <charles.eckman@fastly.com>
2025-06-11 12:40:18 -0400

    Weaken assertion around vCPU startup time: >=0, not >0
    As the comment says, if we're doing _well_ we may have not accumulated a
    full millisecond of startup time.

--- integration-tests/js-compute/fixtures/app/src/compute.js
+++ integration-tests/js-compute/fixtures/app/src/compute.js
@@ -5,7 +5,9 @@ import { purgeSurrogateKey, vCpuTime } from 'fastly:compute';
 routes.set('/compute/get-vcpu-ms', () => {
   const cpuTime = vCpuTime();
   strictEqual(typeof cpuTime, 'number');
-  ok(cpuTime > 0, 'cpuTime > 0');
+  // We can't assert > 0; this only claims millisecond resolution,
+  // and we hopefully spent less than 500us starting.
+  ok(cpuTime >= 0, 'cpuTime > 0');
   ok(cpuTime < 3000, 'cputime < 3000');
   const arr = new Array(100_000).fill(1);
   for (let j = 1; j < 100; j++) {
