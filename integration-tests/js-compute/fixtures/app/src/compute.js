2027609b9638f85c5c633561769e5a9a15a0b3fc
Charles Eckman <charles.eckman@fastly.com>
2025-06-11 12:03:42 -0400

    Formatting fixes

--- integration-tests/js-compute/fixtures/app/src/compute.js
+++ integration-tests/js-compute/fixtures/app/src/compute.js
@@ -5,8 +5,8 @@ import { purgeSurrogateKey, vCpuTime } from 'fastly:compute';
 routes.set('/compute/get-vcpu-ms', () => {
   const cpuTime = vCpuTime();
   strictEqual(typeof cpuTime, 'number');
-  ok(cpuTime > 0, "cpuTime > 0");
-  ok(cpuTime < 3000, "cputime < 3000");
+  ok(cpuTime > 0, 'cpuTime > 0');
+  ok(cpuTime < 3000, 'cputime < 3000');
   const arr = new Array(100_000).fill(1);
   for (let j = 1; j < 100; j++) {
     for (let i = 1; i < 100_000; i++) {
@@ -14,8 +14,8 @@ routes.set('/compute/get-vcpu-ms', () => {
     }
   }
   const cpuTime2 = vCpuTime();
-  ok(cpuTime2 > cpuTime, "cpuTime2 > cpuTime");
-  ok(cpuTime2 - cpuTime > 1, "cpuTime2 - cpuTime > 1");
+  ok(cpuTime2 > cpuTime, 'cpuTime2 > cpuTime');
+  ok(cpuTime2 - cpuTime > 1, 'cpuTime2 - cpuTime > 1');
   return pass('ok');
 });
 
