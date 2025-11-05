a219e1b4576c0040e84aca8d667876e27971f7dc
Sy Brand <sy.brand@fastly.com>
2025-11-05 14:18:40 +0000

    Change test file

--- sy-test/src/index.js
+++ sy-test/src/index.js
@@ -2,15 +2,13 @@
 
 import { Region, Format } from 'fastly:image-optimizer';
 
-
-
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event) {
   return await fetch('https://http-me.glitch.me/image-jpeg', {
     imageOptimizerOptions: {
       region: Region.UsEast,
-      format: Format.PJPG,
+      format: Format.PNG
     },
     backend: 'httpme'
   });
