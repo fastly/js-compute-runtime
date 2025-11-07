6aa7bc3aea75ca4346a69431fdbd10bc9ea1b8f2
Sy Brand <sy.brand@fastly.com>
2025-11-07 11:26:42 +0000

    bgColor working

--- runtime/fastly/builtins/image-optimizer.cpp
+++ runtime/fastly/builtins/image-optimizer.cpp
@@ -12,10 +12,9 @@ const JSPropertySpec EnumOption::properties[] = {
     JS_PS_END,
 };
 
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase)                                  \
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
   const JSPropertySpec type::static_properties[] = {
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value)                                     \
-  JS_STRING_PS(#name, str, JSPROP_READONLY),
+#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str) JS_STRING_PS(#name, str, JSPROP_READONLY),
 #define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)                                               \
   JS_PS_END,                                                                                       \
   }                                                                                                \
@@ -25,7 +24,7 @@ const JSPropertySpec EnumOption::properties[] = {
 bool install(api::Engine *engine) {
   RootedObject image_optimizer_ns(engine->cx(), JS_NewObject(engine->cx(), nullptr));
 
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase)                                  \
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
   if (!type::init_class_impl(engine->cx(), engine->global())) {                                    \
     return false;                                                                                  \
   }                                                                                                \
@@ -38,7 +37,7 @@ bool install(api::Engine *engine) {
   if (!JS_SetProperty(engine->cx(), image_optimizer_ns, #type, lowercase##_val)) {                 \
     return false;                                                                                  \
   }
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value)
+#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str)
 #define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)
 #include "image-optimizer-options.inc"
 
@@ -51,32 +50,25 @@ bool install(api::Engine *engine) {
 
 std::unique_ptr<ImageOptimizerOptions> ImageOptimizerOptions::create(JSContext *cx,
                                                                      JS::HandleValue opts_val) {
-  // Extract properties. `region` is the only mandatory property
-  JS::RootedValue region_val(cx);
+  // Extract properties.
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
+  JS::RootedValue lowercase##_val(cx);
+#include "image-optimizer-options.inc"
   JS::RootedValue preserve_query_string_on_origin_request_val(cx);
-  JS::RootedValue auto_val(cx);
   JS::RootedValue bg_color_val(cx);
   JS::RootedValue blur_val(cx);
   JS::RootedValue brightness_val(cx);
-  JS::RootedValue bw_val(cx);
   JS::RootedValue canvas_val(cx);
   JS::RootedValue contrast_val(cx);
   JS::RootedValue crop_val(cx);
   JS::RootedValue dpr_val(cx);
-  JS::RootedValue enable_val(cx);
-  JS::RootedValue fit_val(cx);
-  JS::RootedValue format_val(cx);
   JS::RootedValue frame_val(cx);
   JS::RootedValue height_val(cx);
   JS::RootedValue level_val(cx);
-  JS::RootedValue metadata_val(cx);
-  JS::RootedValue optimize_val(cx);
   JS::RootedValue orient_val(cx);
   JS::RootedValue pad_val(cx);
   JS::RootedValue precrop_val(cx);
-  JS::RootedValue profile_val(cx);
   JS::RootedValue quality_val(cx);
-  JS::RootedValue resize_filter_val(cx);
   JS::RootedValue saturation_val(cx);
   JS::RootedValue sharpen_val(cx);
   JS::RootedValue trim_val(cx);
@@ -93,7 +85,9 @@ std::unique_ptr<ImageOptimizerOptions> ImageOptimizerOptions::create(JSContext *
       !JS_GetProperty(cx, opts, "bw", &bw_val) ||
       !JS_GetProperty(cx, opts, "canvas", &canvas_val) ||
       !JS_GetProperty(cx, opts, "contrast", &contrast_val) ||
-      !JS_GetProperty(cx, opts, "crop", &crop_val) || !JS_GetProperty(cx, opts, "dpr", &dpr_val) ||
+      !JS_GetProperty(cx, opts, "crop", &crop_val) ||
+      !JS_GetProperty(cx, opts, "disable", &disable_val) ||
+      !JS_GetProperty(cx, opts, "dpr", &dpr_val) ||
       !JS_GetProperty(cx, opts, "enable", &enable_val) ||
       !JS_GetProperty(cx, opts, "fit", &fit_val) ||
       !JS_GetProperty(cx, opts, "format", &format_val) ||
@@ -121,16 +115,24 @@ std::unique_ptr<ImageOptimizerOptions> ImageOptimizerOptions::create(JSContext *
                      "contain region");
     return nullptr;
   }
-  auto region = to_region(cx, region_val);
-  if (!region) {
-    return nullptr;
+
+#define TRY_CONVERT(name)                                                                          \
+  decltype(to_##name(cx, name##_val)) name##_opt;                                                  \
+  if (!name##_val.isUndefined()) {                                                                 \
+    name##_opt = to_##name(cx, name##_val);                                                        \
+    if (!name##_opt) {                                                                             \
+      return nullptr;                                                                              \
+    }                                                                                              \
   }
 
-  auto auto_ = to_auto(cx, auto_val);
-  auto format = to_format(cx, format_val);
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str) TRY_CONVERT(lowercase);
+#include "image-optimizer-options.inc"
 
-  return std::unique_ptr<ImageOptimizerOptions>{
-      new ImageOptimizerOptions(region.value(), auto_, format)};
+  TRY_CONVERT(bg_color);
+
+  return std::unique_ptr<ImageOptimizerOptions>{new ImageOptimizerOptions(
+      *region_opt, auto_opt, std::move(bg_color_opt), bw_opt, disable_opt, enable_opt, fit_opt,
+      format_opt, metadata_opt, optimize_opt, profile_opt, resize_filter_opt)};
 }
 
 std::string ImageOptimizerOptions::to_string() const {
@@ -141,7 +143,16 @@ std::string ImageOptimizerOptions::to_string() const {
       ret += "&" + to_string(*v);
   };
   append(auto_);
+  append(bg_color_);
+  append(bw_);
+  append(disable_);
+  append(enable_);
+  append(fit_);
   append(format_);
+  append(metadata_);
+  append(optimize_);
+  append(profile_);
+  append(resizeFilter_);
   return ret;
 }
 
@@ -153,4 +164,77 @@ fastly_image_optimizer_transform_config ImageOptimizerOptions::to_config() {
   config.sdk_claims_opts_len = host_str.len;
   return config;
 }
+
+std::optional<ImageOptimizerOptions::Color>
+ImageOptimizerOptions::to_color(JSContext *cx, JS::HandleValue val, const std::string &field_name) {
+  // Hex strings of size 3 or 6 are allowed
+  if (val.isString()) {
+    JS::RootedString str_val(cx, val.toString());
+    auto str = core::encode(cx, str_val);
+    if ((str.len == 3 || str.len == 6) &&
+        (std::all_of(str.begin(), str.end(), [](char c) { return std::isxdigit(c); }))) {
+      return Color{std::move(str)};
+    }
+  }
+
+  auto throw_error = [&]() {
+    api::throw_error(cx, api::Errors::TypeError, "imageOptimizerOptions", field_name.data(),
+                     "must be a 3/6 character hex string or RGB(A) object");
+  };
+
+  // Otherwise, it should be an rgb(a) object
+  if (!val.isObject()) {
+    throw_error();
+    return std::nullopt;
+  }
+
+  JS::RootedValue r(cx);
+  JS::RootedValue g(cx);
+  JS::RootedValue b(cx);
+  JS::RootedValue a(cx);
+  JS::RootedObject obj(cx, &val.toObject());
+  if (!JS_GetProperty(cx, obj, "r", &r) || !JS_GetProperty(cx, obj, "g", &g) ||
+      !JS_GetProperty(cx, obj, "b", &b) || !JS_GetProperty(cx, obj, "a", &a)) {
+    return std::nullopt;
+  }
+
+  auto is_valid_color_component = [](const auto &c) {
+    return c.isInt32() && c.toInt32() >= 0 && c.toInt32() < 256;
+  };
+  auto alpha_valid =
+      a.isUndefined() || (a.isNumber() && a.toNumber() >= 0.0 && a.toNumber() <= 1.0);
+  if (!is_valid_color_component(r) || !is_valid_color_component(g) ||
+      !is_valid_color_component(b) || !alpha_valid) {
+    throw_error();
+    return std::nullopt;
+  }
+
+  std::string rep;
+  rep += std::to_string(r.toInt32()) + ',' + std::to_string(g.toInt32()) + ',' +
+         std::to_string(b.toInt32());
+  if (!a.isUndefined()) {
+    rep += ',' + std::to_string(a.toNumber());
+  }
+  return Color{host_api::HostString(rep)};
+}
+
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
+  std::optional<ImageOptimizerOptions::type> ImageOptimizerOptions::to_##lowercase(                \
+      JSContext *cx, JS::HandleValue val) {                                                        \
+    if (!val.isString()) {                                                                         \
+      api::throw_error(cx, api::Errors::TypeError, "imageOptimizerOptions", #type,                 \
+                       "must be a string");                                                        \
+      return std::nullopt;                                                                         \
+    }                                                                                              \
+    JS::RootedString str_val(cx, val.toString());                                                  \
+    using enum type;
+#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str)                                            \
+  if (core::encode(cx, str_val) == std::string_view(str)) {                                        \
+    return name;                                                                                   \
+  }
+#define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)                                               \
+  JS_ReportErrorUTF8(cx, #type " out of range");                                                   \
+  return std::nullopt;                                                                             \
+  }
+#include "image-optimizer-options.inc"
 } // namespace fastly::image_optimizer
--- runtime/fastly/builtins/image-optimizer.h
+++ runtime/fastly/builtins/image-optimizer.h
@@ -18,14 +18,12 @@ public:
   static const JSFunctionSpec methods[];
   static const JSPropertySpec properties[];
 };
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase)                                  \
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
   class type : public EnumOption, public builtins::BuiltinNoConstructor<type> {                    \
   public:                                                                                          \
     static const JSPropertySpec static_properties[];                                               \
     static constexpr const char *class_name = #type;                                               \
   };
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value)
-#define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)
 #include "image-optimizer-options.inc"
 
 class ImageOptimizerOptions {
@@ -34,50 +32,65 @@ public:
   static std::unique_ptr<ImageOptimizerOptions> create(JSContext *cx, JS::HandleValue opts_val);
   std::string to_string() const;
 
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase) enum class type {
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value) name = value,
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str) enum class type {
+#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str) name,
 #define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)                                               \
   }                                                                                                \
   ;
 #include "image-optimizer-options.inc"
 
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase)                                  \
-  static std::optional<type> to_##lowercase(JSContext *cx, JS::HandleValue val) {                  \
-    if (val.isUndefined())                                                                         \
-      return std::nullopt;                                                                         \
-    if (!val.isString()) {                                                                         \
-      api::throw_error(cx, api::Errors::TypeError, "imageOptimizerOptions", #type,                 \
-                       "must be a string");                                                        \
-      return std::nullopt;                                                                         \
-    }                                                                                              \
-    JS::RootedString str_val(cx, val.toString());                                                  \
-    using enum type;
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value)                                     \
-  if (core::encode(cx, str_val) == std::string_view(str)) {                                        \
-    return name;                                                                                   \
-  }
-#define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)                                               \
-  JS_ReportErrorUTF8(cx, #type " out of range");                                                   \
-  return std::nullopt;                                                                             \
-  }
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
+  static std::optional<type> to_##lowercase(JSContext *cx, JS::HandleValue val);
 #include "image-optimizer-options.inc"
 
+  struct Color {
+    host_api::HostString val;
+  };
+  static std::optional<Color> to_color(JSContext *cx, JS::HandleValue val,
+                                       const std::string &field_name);
+
+  struct BGColor {
+    Color color;
+  };
+  static std::optional<BGColor> to_bg_color(JSContext *cx, JS::HandleValue val) {
+    auto color = to_color(cx, val, "bgColor");
+    if (!color)
+      return std::nullopt;
+    return BGColor{std::move(*color)};
+  }
+
 private:
-  ImageOptimizerOptions(Region region, std::optional<Auto> auto_val, std::optional<Format> format)
-      : region_(region), auto_(auto_val), format_(format) {}
+  ImageOptimizerOptions(Region region, std::optional<Auto> auto_val,
+                        std::optional<BGColor> bg_color, std::optional<BWAlgorithm> bw,
+                        std::optional<Disable> disable, std::optional<Enable> enable,
+                        std::optional<Fit> fit, std::optional<Format> format,
+                        std::optional<Metadata> metadata, std::optional<Optimize> optimize,
+                        std::optional<Profile> profile, std::optional<ResizeFilter> resize_filter)
+      : region_(region), auto_(auto_val), bg_color_(std::move(bg_color)), bw_(bw),
+        disable_(disable), enable_(enable), fit_(fit), format_(format), metadata_(metadata),
+        optimize_(optimize), profile_(profile), resizeFilter_(resize_filter) {}
   Region region_;
   std::optional<Auto> auto_;
+  std::optional<BGColor> bg_color_;
+  std::optional<BWAlgorithm> bw_;
+  std::optional<Disable> disable_;
+  std::optional<Enable> enable_;
+  std::optional<Fit> fit_;
   std::optional<Format> format_;
+  std::optional<Metadata> metadata_;
+  std::optional<Optimize> optimize_;
+  std::optional<Profile> profile_;
+  std::optional<ResizeFilter> resizeFilter_;
 };
 
 inline std::string to_string(const ImageOptimizerOptions &opts) { return opts.to_string(); }
 
-#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase)                                  \
+#define FASTLY_BEGIN_IMAGE_OPTIMIZER_OPTION_TYPE(type, lowercase, str)                             \
   inline std::string to_string(ImageOptimizerOptions::type val) {                                  \
     using enum ImageOptimizerOptions::type;                                                        \
-    std::string prefix = #lowercase;                                                               \
+    std::string prefix = str;                                                                      \
     switch (val) {
-#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str, value)                                     \
+#define FASTLY_DEFINE_IMAGE_OPTIMIZER_OPTION(name, str)                                            \
   case name:                                                                                       \
     return prefix + '=' + str;
 #define FASTLY_END_IMAGE_OPTIMIZER_OPTION_TYPE(type)                                               \
@@ -85,5 +98,11 @@ inline std::string to_string(const ImageOptimizerOptions &opts) { return opts.to
   }
 #include "image-optimizer-options.inc"
 
+inline std::string to_string(const ImageOptimizerOptions::Color &c) {
+  return std::string{std::string_view(c.val)};
+}
+inline std::string to_string(const ImageOptimizerOptions::BGColor &bg) {
+  return "bg-color=" + to_string(bg.color);
+}
 } // namespace fastly::image_optimizer
 #endif
