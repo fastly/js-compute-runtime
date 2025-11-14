5e15d4586f627a21e7a5f0a7e683cc6256155b63
Sy Brand <sy.brand@fastly.com>
2025-11-14 13:35:41 +0000

    Remove resources even if tests fail

--- integration-tests/js-compute/test.js
+++ integration-tests/js-compute/test.js
@@ -136,7 +136,7 @@ if (!local) {
   core.startGroup('Delete service if already exists');
   try {
     await zx`fastly service delete --quiet --service-name "${serviceName}" --force --token $FASTLY_API_TOKEN`;
-  } catch {}
+  } catch { }
   core.endGroup();
   core.startGroup('Build and deploy service');
   await zx`npm i`;
@@ -170,13 +170,13 @@ await Promise.all([
       27,
       local
         ? [
-            // we expect it to take ~10 seconds to deploy, so focus on that time
-            6000, 3000, 1500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
-            500, 500, 500, 500, 500, 500, 500, 500, 500,
-            // after more than 20 seconds, means we have an unusually slow build, start backoff before timeout
-            1500,
-            3000, 6000, 12000, 24000,
-          ].values()
+          // we expect it to take ~10 seconds to deploy, so focus on that time
+          6000, 3000, 1500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+          500, 500, 500, 500, 500, 500, 500, 500, 500,
+          // after more than 20 seconds, means we have an unusually slow build, start backoff before timeout
+          1500,
+          3000, 6000, 12000, 24000,
+        ].values()
         : expBackoff('60s', '10s'),
       async () => {
         const response = await request(domain);
@@ -275,12 +275,12 @@ for (const chunk of chunks(Object.entries(tests), 100)) {
                 case 'first-chunk-only':
                   for await (const chunk of response.body) {
                     bodyChunks.push(chunk);
-                    response.body.on('error', () => {});
+                    response.body.on('error', () => { });
                     break;
                   }
                   break;
                 case 'none':
-                  response.body.on('error', () => {});
+                  response.body.on('error', () => { });
                   break;
                 case 'full':
                 default:
@@ -435,7 +435,7 @@ if (!local && failed.length) {
   core.notice(`Tests failed.`);
 }
 
-if (!local && !skipTeardown && failed.length === 0) {
+if (!local && !skipTeardown) {
   const teardownPath = join(__dirname, 'teardown.js');
   if (existsSync(teardownPath)) {
     core.startGroup('Tear down the extra set-up for the service');
