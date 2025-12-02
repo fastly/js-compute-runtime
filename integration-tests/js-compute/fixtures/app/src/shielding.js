94357a6908a6e3d17eac44f7e869c1311deb29ba
Sy Brand <sy.brand@fastly.com>
2025-12-02 15:44:32 +0000

    Docs and testing

--- integration-tests/js-compute/fixtures/app/src/shielding.js
+++ integration-tests/js-compute/fixtures/app/src/shielding.js
@@ -1,4 +1,4 @@
-import { pass, ok, strictEqual, assertThrows } from './assertions.js';
+import { assert, assertThrows } from './assertions.js';
 import { routes } from './routes.js';
 import { Shield } from 'fastly:shielding';
 
@@ -8,14 +8,10 @@ routes.set('/shielding/encrypted', async (event) => {
     if (shield.runningOn()) {
         return await fetch('https://http-me.glitch.me/anything', { backend: 'httpme' });
     }
-    return await fetch(event.request, { backend: shield.encryptedBackend() });
+    let resp = await fetch(event.request, { backend: shield.encryptedBackend() });
+    assert(resp.headers.get('x-cache').includes(','), true, 'headers[x-cache].includes(",")');
 });
 
-routes.set('/shielding/unencrypted', async (event) => {
-    // If you're running these tests from Sydney, this will fail
-    let shield = new Shield('wsi-australia-au');
-    if (shield.runningOn()) {
-        return await fetch('https://http-me.glitch.me/anything', { backend: 'httpme' });
-    }
-    return await fetch(event.request, { backend: shield.unencryptedBackend() });
+routes.set('/shielding/invalid-shield', () => {
+    assertThrows(new Shield('i-am-not-a-real-shield'));
 });
