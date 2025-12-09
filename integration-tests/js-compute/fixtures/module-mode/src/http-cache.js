e0ebbbe76d9105ea2a04036f9d6c773ea1a5f425
Sy Brand <sy.brand@fastly.com>
2025-12-09 16:55:06 +0000

    Format

--- integration-tests/js-compute/fixtures/module-mode/src/http-cache.js
+++ integration-tests/js-compute/fixtures/module-mode/src/http-cache.js
@@ -466,7 +466,7 @@ const getTestUrl = (path = `/${Math.random().toString().slice(2)}`) =>
     });
     strictEqual(calledAfterSend, true);
     // verify we get a proper response (url included in response)
-    strictEqual('https://http-me.glitch.me/' + (await res.json()).url, url);
+    strictEqual('https://http-me.glitch.me' + (await res.json()).url, url);
   });
 
   // Test response property mutations
