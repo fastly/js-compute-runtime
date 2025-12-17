eaeba0e69f7c8448f6777b33e1abb661eedfd287
Sy Brand <sy.brand@fastly.com>
2025-12-17 12:01:19 +0000

    Format

--- integration-tests/js-compute/fixtures/app/src/request-body.js
+++ integration-tests/js-compute/fixtures/app/src/request-body.js
@@ -3,17 +3,17 @@
 import { routes } from './routes.js';
 
 routes.set('/request/body/proxy-transform-stream', async (event) => {
-    const incomingReq = new Request('https://example.com', {
-        body: event.request.body,
-        method: "POST"
-    });
-    const newReq = new Request(incomingReq);
-    let bodyText = '';
-    const reader = newReq.body.getReader();
-    while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-        bodyText += new TextDecoder().decode(value);
-    }
-    return new Response(bodyText, { status: 200 });
+  const incomingReq = new Request('https://example.com', {
+    body: event.request.body,
+    method: 'POST',
+  });
+  const newReq = new Request(incomingReq);
+  let bodyText = '';
+  const reader = newReq.body.getReader();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    bodyText += new TextDecoder().decode(value);
+  }
+  return new Response(bodyText, { status: 200 });
 });
