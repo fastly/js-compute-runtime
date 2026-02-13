b929648bef34c0d41fe387fc755a2554372bb946
Kat Marchán <kzm@zkat.tech>
2026-02-13 01:14:04 -0800

    fix: Body truncation error in chained extract_body requests (#1310)
    Co-authored-by: Sy Brand <sy.brand@fastly.com>
    Co-authored-by: Sy Brand <tartanllama@gmail.com>

--- integration-tests/js-compute/fixtures/app/src/proxy-transform-stream.js
+++ integration-tests/js-compute/fixtures/app/src/proxy-transform-stream.js
@@ -5,28 +5,24 @@
 import { assert, assertThrows } from './assertions.js';
 import { routes } from './routes.js';
 
-routes.set('/proxy-transform-stream/content-length/backend', async (event) => {
-  assert(
-    event.request.headers.get('content-length'),
-    '11',
-    'Content-Length header should be 11',
-  );
-  return new Response('ok');
-});
-
 routes.set('/proxy-transform-stream/content-length', async (event) => {
   let request = new Request(
-    '/proxy-transform-stream/content-length/backend',
+    'https://http-me.fastly.dev/anything',
     event.request,
   );
   let proxy = new Request(request);
   const response = await fetch(proxy);
-  assert(response.status, 200, 'Response status should be 200');
+  assert(response.status, 200, 'status == 200');
+  assert(
+    (await response.json()).headers['content-length'],
+    '11',
+    'headers["content-length"] == 11',
+  );
   return new Response('ok');
 });
 
 routes.set(
-  '/proxy-transform-stream/simple-response-body-chain/backend',
+  '/proxy-transform-stream/simple-response-body-chain',
   async (event) => {
     const res = new Response('body');
     return new Response(res.body, res);
@@ -34,67 +30,49 @@ routes.set(
 );
 
 routes.set(
-  '/proxy-transform-stream/simple-response-body-chain',
-  async (event) => {
-    const req = new Request(
-      '/proxy-transform-stream/simple-response-body-chain/backend',
-    );
-    const res = await fetch(req);
-    assert(res.status, 200, 'Status should be 200');
-    assert(
-      res.headers.get('content-length'),
-      '4',
-      'Should use Content-Length instead of Transfer-Encoding',
-    );
-    return new Response('ok');
-  },
-);
-
-routes.set(
-  '/proxy-transform-stream/multi-response-body-chain/backend',
+  '/proxy-transform-stream/multi-response-body-chain',
   async (event) => {
     let res = new Response('body');
     res = new Response(res.body, res);
-    return new Response(res.body, res);
-  },
-);
-
-routes.set(
-  '/proxy-transform-stream/multi-response-body-chain',
-  async (event) => {
-    const req = new Request(
-      '/proxy-transform-stream/multi-response-body-chain/backend',
-    );
-    const res = await fetch(req);
-    assert(res.status, 200, 'Status should be 200');
-    assert(
-      res.headers.get('content-length'),
-      '4',
-      'Should use Content-Length instead of Transfer-Encoding',
-    );
-    return new Response('ok');
-  },
-);
-
-routes.set(
-  '/proxy-transform-stream/response-body-into-js/backend',
-  async (event) => {
-    let res = new Response('body');
-    const body = await res.text();
-    return new Response(body, res);
+    res = new Response(res.body, res);
+    return res;
   },
 );
 
 routes.set('/proxy-transform-stream/response-body-into-js', async (event) => {
-  const req = new Request(
-    '/proxy-transform-stream/response-body-into-js/backend',
-  );
-  const res = await fetch(req);
+  let res = new Response('body');
+  const body = await res.text();
+  return new Response(body, res);
+});
+
+routes.set('/proxy-transform-stream/framing', async (event) => {
+  const newUrl = new URL('https://http-me.fastly.dev/anything');
+  let req = new Request(newUrl, {
+    headers: event.request.headers,
+    body: event.request.body,
+    method: event.request.method,
+  });
+  req = new Request(req.url, {
+    headers: req.headers,
+    body: req.body,
+    method: req.method,
+  });
+  req = new Request(req.url, {
+    headers: req.headers,
+    body: req.body,
+    method: req.method,
+  });
+  const cacheOverride = new CacheOverride('pass');
+  const res = await fetch(req, {
+    backend: 'httpme',
+    cacheOverride,
+  });
+  let json = await res.json();
   assert(res.status, 200, 'Status should be 200');
   assert(
-    res.headers.get('content-length'),
-    '4',
-    'Should use Content-Length instead of Transfer-Encoding',
+    json['headers']['content-length'],
+    '11',
+    'Content-Length header should be 11',
   );
   return new Response('ok');
 });
