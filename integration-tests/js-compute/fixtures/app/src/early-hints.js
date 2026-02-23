5c8b5d53ba5d568c52834e41659b32cddadc952f
Kat Marchán <kat.marchan@fastly.com>
2026-02-23 15:15:34 -0800

    fix(early-hints): early hints don't need to be sync inside a FetchEvent handler
    Fixes: https://github.com/fastly/js-compute-runtime/issues/1322

--- integration-tests/js-compute/fixtures/app/src/early-hints.js
+++ integration-tests/js-compute/fixtures/app/src/early-hints.js
@@ -11,9 +11,54 @@ routes.set('/early-hints/manual-response', (event) => {
   event.respondWith(new Response('ok'));
 });
 
+routes.set('/early-hints/manual-response-late', (event) => {
+  event.respondWith(new Response('ok'));
+  assertThrows(() => {
+    event.respondWith(
+      new Response(null, {
+        status: 103,
+        headers: { link: '</style.css>; rel=preload; as=style' },
+      }),
+    );
+  });
+});
+
+routes.set('/early-hints/manual-response-async', async (event) => {
+  await (async () => {
+    event.respondWith(
+      new Response(null, {
+        status: 103,
+        headers: { link: '</style.css>; rel=preload; as=style' },
+      }),
+    );
+    event.respondWith(new Response('ok'));
+  })();
+});
+
+routes.set('/early-hints/manual-response-late-async', async (event) => {
+  await (async () => {
+    event.respondWith(new Response('ok'));
+    assertThrows(() => {
+      event.respondWith(
+        new Response(null, {
+          status: 103,
+          headers: { link: '</style.css>; rel=preload; as=style' },
+        }),
+      );
+    });
+  })();
+});
+
 routes.set('/early-hints/send-early-hints', (event) => {
   event.sendEarlyHints({ link: '</style.css>; rel=preload; as=style' });
   event.respondWith(new Response('ok'));
+});
+
+routes.set('/early-hints/send-early-hints-late', (event) => {
+  event.respondWith(new Response('ok'));
+  assertThrows(() => {
+    event.sendEarlyHints({ link: '</style.css>; rel=preload; as=style' });
+  });
 });
 
 routes.set('/early-hints/send-early-hints-multiple-headers', (event) => {
@@ -22,4 +67,20 @@ routes.set('/early-hints/send-early-hints-multiple-headers', (event) => {
     ['link', '</style2.css>; rel=preload; as=style'],
   ]);
   event.respondWith(new Response('ok'));
+});
+
+routes.set('/early-hints/send-early-hints-async', async (event) => {
+  await (async () => {
+    event.sendEarlyHints({ link: '</style.css>; rel=preload; as=style' });
+    event.respondWith(new Response('ok'));
+  })();
+});
+
+routes.set('/early-hints/send-early-hints-late-async', async (event) => {
+  await (async () => {
+    event.respondWith(new Response('ok'));
+    assertThrows(() => {
+      event.sendEarlyHints({ link: '</style.css>; rel=preload; as=style' });
+    });
+  })();
 });
