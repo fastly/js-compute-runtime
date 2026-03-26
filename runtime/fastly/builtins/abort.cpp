9582ef1ffc76a369ac658e1f1b0ac23fe6cd6170
Sy Brand <sy.brand@fastly.com>
2026-03-26 13:48:41 +0000

    fmt

--- runtime/fastly/builtins/abort.cpp
+++ runtime/fastly/builtins/abort.cpp
@@ -1,24 +1,18 @@
 
 #include "../../StarlingMonkey/builtins/web/abort/abort-signal.h"
 
-// StarlingMonkey's event builtin depends on parts of the abort builtin, but we don't support aborting
-// fetches yet, so we don't build the abort builtin at all. To get around this, we stub the relevant
-// abort signal methods that the event builtin depends on.
+// StarlingMonkey's event builtin depends on parts of the abort builtin, but we don't support
+// aborting fetches yet, so we don't build the abort builtin at all. To get around this, we stub the
+// relevant abort signal methods that the event builtin depends on.
 
 namespace builtins::web::abort {
-bool AbortSignal::add_algorithm(JSObject *, js::UniquePtr<AbortAlgorithm>) {
-    return true;
-}
+bool AbortSignal::add_algorithm(JSObject *, js::UniquePtr<AbortAlgorithm>) { return true; }
 
-bool AbortSignal::is_aborted(JSObject *) {
-    return false;
-}
+bool AbortSignal::is_aborted(JSObject *) { return false; }
 
-void AbortSignal::finalize(JS::GCContext *, JSObject *){}
-void AbortSignal::trace(JSTracer *, JSObject *){}
-}
+void AbortSignal::finalize(JS::GCContext *, JSObject *) {}
+void AbortSignal::trace(JSTracer *, JSObject *) {}
+} // namespace builtins::web::abort
 namespace fastly::abort {
-bool install(api::Engine *engine) {
-  return true;
-}
-}
+bool install(api::Engine *engine) { return true; }
+} // namespace fastly::abort
