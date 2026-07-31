da5146c04e2d872fbc166852048a4ec05cd82584
Sy Brand <tartanllama@gmail.com>
2026-07-31 16:48:18 +0100

    fix: URLSearchParams premature free (#1538)

--- integration-tests/js-compute/fixtures/app/src/urlsearchparams.js
+++ integration-tests/js-compute/fixtures/app/src/urlsearchparams.js
@@ -7,3 +7,27 @@ routes.set('/urlsearchparams/sort', async () => {
   urlObj.searchParams.sort();
   assert(urlObj.toString(), 'http://www.example.com/', `urlObj.toString()`);
 });
+
+// A regression test for a premature free that occurred when the URL associated
+// with a URLSearchParams object went out of scope. This would fail at high GC zeal.
+routes.set('/urlsearchparams/temp-url', async () => {
+  const searchParams = (() => {
+    return new URL('http://www.example.com?a=b').searchParams;
+  })();
+
+  new URL('http://www.do-some-allocations.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+  new URL('http://www.to-force-the-above-url-to-be-gced.com').searchParams;
+
+  assert(searchParams.get('a'), 'b', "searchParams.get('a') == 'b'");
+});
