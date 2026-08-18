430c70d01f9186124d528e280162d8f29beaf6f0
Sy Brand <sy.brand@fastly.com>
2026-08-18 12:56:36 +0100

    Remove unnecessary changes

--- runtime/fastly/host-api/fastly.h
+++ runtime/fastly/host-api/fastly.h
@@ -577,10 +577,6 @@ WASM_IMPORT("fastly_http_downstream", "downstream_tls_client_servername")
 int http_downstream_tls_client_servername(uint32_t req_handle, uint8_t *ret, size_t ret_len,
                                      size_t *nwritten);
 
-WASM_IMPORT("fastly_http_downstream", "downstream_tls_client_hello")
-int http_downstream_tls_client_hello(uint32_t req_handle, uint8_t *ret, size_t ret_len,
-                                     size_t *nwritten);
-
 WASM_IMPORT("fastly_http_downstream", "downstream_tls_raw_client_certificate")
 int http_downstream_tls_raw_client_certificate(uint32_t req_handle, uint8_t *ret, size_t ret_len,
                                                size_t *nwritten);
