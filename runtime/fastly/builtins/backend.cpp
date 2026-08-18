f052a359c79304ccf0aa1ffdf4646d36e58ec981
Sy Brand <sy.brand@fastly.com>
2026-08-18 11:43:03 +0100

    Fix setBoolean call

--- runtime/fastly/builtins/backend.cpp
+++ runtime/fastly/builtins/backend.cpp
@@ -1573,7 +1573,7 @@ bool Backend::is_ssl_get(JSContext *cx, unsigned argc, JS::Value *vp) {
   }
   auto res = backend->is_ssl();
   if (auto *err = res.to_err()) {
-    args.rval().setBoolean();
+    args.rval().setBoolean(false);
     return true;
   }
   args.rval().setBoolean(res.unwrap());
