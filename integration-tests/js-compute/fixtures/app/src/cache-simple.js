65393ebef6ffda80b0fb26726e0843ad662fa79e
Sy Brand <sy.brand@fastly.com>
2026-09-03 13:07:41 +0100

    fmt

--- integration-tests/js-compute/fixtures/app/src/cache-simple.js
+++ integration-tests/js-compute/fixtures/app/src/cache-simple.js
@@ -2210,24 +2210,21 @@ async function simpleCacheEntryInterfaceTests() {
       }
     },
   );
-  routes.set(
-    '/simple-cache/getOrSet/commit-on-success',
-    async () => {
-      if (!isRunningLocally()) {
-        let key = String(Math.random()) + 'commit';
-        await SimpleCache.getOrSet(key, async () => {
-          return {
-            value: key,
-            ttl: 10000,
-          };
-        });
-        await SimpleCache.getOrSet(key, async () => {
-          return {
-            value: key,
-            ttl: 10000,
-          };
-        });
-      }  
+  routes.set('/simple-cache/getOrSet/commit-on-success', async () => {
+    if (!isRunningLocally()) {
+      let key = String(Math.random()) + 'commit';
+      await SimpleCache.getOrSet(key, async () => {
+        return {
+          value: key,
+          ttl: 10000,
+        };
+      });
+      await SimpleCache.getOrSet(key, async () => {
+        return {
+          value: key,
+          ttl: 10000,
+        };
+      });
     }
-  );
+  });
 }
